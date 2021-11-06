466e1fedfa3dc5a68c38689e88953c46edccb165
Chakit Gupta <76941922+chakitg@users.noreply.github.com>
2021-11-06 17:55:11 +0530

    added code for Single number III

---
+++ Leetcode_Sol/Single_Number_III_C++.cpp
@@ -0,0 +1,58 @@
+// Given an integer array nums, in which exactly two elements appear only once and all the other elements appear exactly twice. Find the two elements that appear only once. You can return the answer in any order.
+
+// You must write an algorithm that runs in linear runtime complexity and uses only constant extra space.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,2,1,3,2,5]
+// Output: [3,5]
+// Explanation:  [5, 3] is also a valid answer.
+// Example 2:
+
+// Input: nums = [-1,0]
+// Output: [-1,0]
+// Example 3:
+
+// Input: nums = [0,1]
+// Output: [1,0]
+ 
+
+// Constraints:
+
+// 2 <= nums.length <= 3 * 104
+// -231 <= nums[i] <= 231 - 1
+// Each integer in nums will appear twice, only two integers will appear once.
+
+
+
+
+class Solution {
+public:
+    vector<int> singleNumber(vector<int> & nums) {
+        int allxor = 0;
+        for (int n : nums) {
+            allxor ^= n;
+        }
+        int mask = 1;
+        while ( (mask & allxor) == 0 ) {
+            mask <<= 1;
+        }
+
+        int zero = 0;
+        int one = 0;
+        for (int n : nums) {
+            if (n & mask ){
+                one ^= n;
+            }else {
+                zero ^= n;
+            }
+        }
+        
+        vector<int> result;
+        result.push_back(zero);
+        result.push_back(one);
+        return result;
+    }
+};
