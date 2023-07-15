efc2b36481b5c64001750ef5b04828adbb27027d
Chakit Gupta <76941922+chakitg@users.noreply.github.com>
2023-07-15 22:51:09 +0530

    Create Delete middle element of a stack.cpp

---
+++ GFG_Sol/Delete middle element of a stack.cpp
@@ -0,0 +1,58 @@
+Given a stack, delete the middle element of the stack without using any additional data structure.
+Middle element:- ceil((size_of_stack+1)/2) (1-based indexing) from bottom of the stack.
+
+Note: The output shown by the compiler is the stack from top to bottom.
+ 
+Example 1:
+
+Input: 
+Stack = {10, 20, 30, 40, 50}
+Output:
+ModifiedStack = {10, 20, 40, 50}
+Explanation:
+If you print the stack the bottom-most element will be 10 and the top-most element will be 50. Middle element will be element at index 3 from bottom, which is 30. Deleting 30, stack will look like {10 20 40 50}.
+Example 2:
+
+Input: 
+Stack = {10 20 30 40}
+Output:
+ModifiedStack = {10 30 40}
+Explanation:
+If you print the stack the bottom-most element will be 10 and the top-most element will be 40. Middle element will be element at index 2 from bottom, which is 20. Deleting 20, stack will look like {10 30 40}.
+Your Task:
+You don't need to read input or print anything. Complete the function deleteMid() which takes the stack and its size as input parameters and modifies the stack in-place.
+
+Expected Time Complexity: O(N)
+Expected Auxiliary Space: O(N)
+
+Constraints:
+2 ≤ size of stack ≤ 105
+
+
+
+
+class Solution
+{
+    public:
+    //Function to delete middle element of a stack.
+    void deleteMid(stack<int>&s, int si)
+    {
+        int i=0;
+        int n=si/2;
+        stack<int>s1;
+        while(i<n)
+        {
+            int a=s.top();
+            s.pop();
+            s1.push(a);
+            i++;
+        }
+        s.pop();
+        
+        while(!s1.empty())
+        {
+        int a=s1.top();s1.pop();
+        s.push(a);
+        }
+    }
+};
