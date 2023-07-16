2e4d95f8f0e05ee3f92ab69686f169c28bf736ce
Chakit Gupta <76941922+chakitg@users.noreply.github.com>
2023-07-16 23:50:10 +0530

    Create 1207. Unique Number of Occurrences.cpp

---
+++ Leetcode_Sol/1207. Unique Number of Occurrences.cpp
@@ -0,0 +1,44 @@
+Given an array of integers arr, return true if the number of occurrences of each value in the array is unique or false otherwise.
+
+ 
+
+Example 1:
+
+Input: arr = [1,2,2,1,1,3]
+Output: true
+Explanation: The value 1 has 3 occurrences, 2 has 2 and 3 has 1. No two values have the same number of occurrences.
+Example 2:
+
+Input: arr = [1,2]
+Output: false
+Example 3:
+
+Input: arr = [-3,0,1,-3,1,1,1,-3,10,0]
+Output: true
+ 
+
+Constraints:
+
+1 <= arr.length <= 1000
+-1000 <= arr[i] <= 1000
+
+
+
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        map<int,int>mp;
+        bool ok = 1;
+        for(auto x : arr) mp[x]++;
+        set<int>s;
+        for(auto x : mp) {
+            if(s.find(x.second) == s.end()) {
+                s.insert(x.second);
+            }
+            else {
+                ok = 0;
+            }
+        }
+        return ok;
+    }
+};
