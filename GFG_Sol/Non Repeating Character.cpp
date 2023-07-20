08f8807ae91d0972bc818016c6d890be2951583a
Chakit Gupta <76941922+chakitg@users.noreply.github.com>
2023-07-20 23:56:59 +0530

    Create Non Repeating Character.cpp

---
+++ GFG_Sol/Non Repeating Character.cpp
@@ -0,0 +1,50 @@
+Given a string S consisting of lowercase Latin Letters. Return the first non-repeating character in S. If there is no non-repeating character, return '$'.
+
+Example 1:
+
+Input:
+S = hello
+Output: h
+Explanation: In the given string, the
+first character which is non-repeating
+is h, as it appears first and there is
+no other 'h' in the string.
+Example 2:
+
+Input:
+S = zxvczbtxyzvy
+Output: c
+Explanation: In the given string, 'c' is
+the character which is non-repeating. 
+Your Task:
+You only need to complete the function nonrepeatingCharacter() that takes string S as a parameter and returns the character. If there is no non-repeating character then return '$' .
+
+Expected Time Complexity: O(N).
+Expected Auxiliary Space: O(Number of distinct characters).
+Note: N = |S|
+
+Constraints:
+1 <= N <= 105
+
+
+
+
+
+class Solution
+{
+    public:
+    //Function to find the first non-repeating character in a string.
+    char nonrepeatingCharacter(string S)
+    {
+       //Your code here
+       unordered_map<char,int> m;
+       for(auto i:S) m[i]++;
+       for(auto i:S)
+       {
+           if(m[i]==1) return i;
+       }
+       char c='$';
+       return c;
+    }
+
+};
