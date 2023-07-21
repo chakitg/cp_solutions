66d70a42cf8ff50cf27036347224194814a3f3ac
Chakit Gupta <76941922+chakitg@users.noreply.github.com>
2023-07-21 23:55:23 +0530

    Create 673. Number of Longest Increasing Subsequence.cpp

---
+++ Leetcode_Sol/673. Number of Longest Increasing Subsequence.cpp
@@ -0,0 +1,62 @@
+Given an integer array nums, return the number of longest increasing subsequences.
+
+Notice that the sequence has to be strictly increasing.
+
+ 
+
+Example 1:
+
+Input: nums = [1,3,5,4,7]
+Output: 2
+Explanation: The two longest increasing subsequences are [1, 3, 4, 7] and [1, 3, 5, 7].
+Example 2:
+
+Input: nums = [2,2,2,2,2]
+Output: 5
+Explanation: The length of the longest increasing subsequence is 1, and there are 5 increasing subsequences of length 1, so output 5.
+ 
+
+Constraints:
+
+1 <= nums.length <= 2000
+-106 <= nums[i] <= 106
+
+
+
+class Solution {
+public:
+    int findNumberOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 0) return 0;
+
+        vector<int> lengths(n, 1); 
+        vector<int> counts(n, 1); 
+        vector<int> bit(n + 1, 0); 
+
+        int max_length = 1; 
+
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                if (nums[i] > nums[j]) {
+                    if (lengths[j] + 1 > lengths[i]) {
+                        lengths[i] = lengths[j] + 1;
+                        counts[i] = counts[j];
+                    } else if (lengths[j] + 1 == lengths[i]) {
+                        counts[i] += counts[j];
+                    }
+                }
+            }
+            max_length = max(max_length, lengths[i]);
+        }
+
+        int result = 0;
+        for (int i = 0; i < n; ++i) {
+            if (lengths[i] == max_length) {
+                result += counts[i];
+            }
+        }
+
+        return result;
+        
+    }
+};
