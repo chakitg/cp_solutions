a4f8c9c9c4b06945cf2e0e64b45f10ceb1f88bd5
Chakit Gupta <76941922+chakitg@users.noreply.github.com>
2023-07-24 22:27:55 +0530

    Create 50. Pow(x, n).cpp

---
+++ Leetcode_Sol/50. Pow(x, n).cpp
@@ -0,0 +1,59 @@
+Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
+
+ 
+
+Example 1:
+
+Input: x = 2.00000, n = 10
+Output: 1024.00000
+Example 2:
+
+Input: x = 2.10000, n = 3
+Output: 9.26100
+Example 3:
+
+Input: x = 2.00000, n = -2
+Output: 0.25000
+Explanation: 2-2 = 1/22 = 1/4 = 0.25
+ 
+
+Constraints:
+
+-100.0 < x < 100.0
+-231 <= n <= 231-1
+n is an integer.
+Either x is not zero or n > 0.
+-104 <= xn <= 104
+
+
+
+
+
+  class Solution {
+public:
+    double binaryExp(double x, long long n) {
+        // Base case, to stop recursive calls.
+        if (n == 0) {
+            return 1;
+        }
+       
+        // Handle case where, n < 0.
+        if (n < 0) {
+            return 1.0 / binaryExp(x, -1 * n);
+        }
+       
+        // Perform Binary Exponentiation.
+        // If 'n' is odd we perform Binary Exponentiation on 'n - 1' and multiply result with 'x'.
+        if (n % 2 == 1) {
+            return x * binaryExp(x * x, (n - 1) / 2);
+        }
+        // Otherwise we calculate result by performing Binary Exponentiation on 'n'.
+        else {
+            return binaryExp(x * x, n / 2);
+        }
+    }
+
+    double myPow(double x, int n) {
+        return binaryExp(x, (long long) n);
+    }
+};
