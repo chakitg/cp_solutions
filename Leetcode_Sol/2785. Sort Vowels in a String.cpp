bceadd30c77da9d6c26f12d6730d78be108bee76
Chakit Gupta <76941922+chakitg@users.noreply.github.com>
2023-07-27 22:41:58 +0530

    Create 2785. Sort Vowels in a String.cpp

---
+++ Leetcode_Sol/2785. Sort Vowels in a String.cpp
@@ -0,0 +1,52 @@
+Given a 0-indexed string s, permute s to get a new string t such that:
+
+All consonants remain in their original places. More formally, if there is an index i with 0 <= i < s.length such that s[i] is a consonant, then t[i] = s[i].
+The vowels must be sorted in the nondecreasing order of their ASCII values. More formally, for pairs of indices i, j with 0 <= i < j < s.length such that s[i] and s[j] are vowels, then t[i] must not have a higher ASCII value than t[j].
+Return the resulting string.
+
+The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in lowercase or uppercase. Consonants comprise all letters that are not vowels.
+
+ 
+
+Example 1:
+
+Input: s = "lEetcOde"
+Output: "lEOtcede"
+Explanation: 'E', 'O', and 'e' are the vowels in s; 'l', 't', 'c', and 'd' are all consonants. The vowels are sorted according to their ASCII values, and the consonants remain in the same places.
+Example 2:
+
+Input: s = "lYmpH"
+Output: "lYmpH"
+Explanation: There are no vowels in s (all characters in s are consonants), so we return "lYmpH".
+ 
+
+Constraints:
+
+1 <= s.length <= 105
+s consists only of letters of the English alphabet in uppercase and lowercase.
+
+
+
+
+class Solution {
+public:
+    string sortVowels(string s) {
+        vector<char>a;
+        int n = (int)s.size();
+        set<char>v = {'a', 'e', 'i', 'o','u','A','E','I','O','U'};
+        for(auto x : s) {
+          if(v.find(x) != v.end()) {
+              a.push_back(x);
+          }
+        }
+        sort(a.begin(),a.end());
+        int j = 0;
+        for(int i = 0 ; i < n ; i++) {
+            if(v.find(s[i]) != v.end()) {
+                s[i] = a[j];
+                j++;
+            }
+        }
+        return s;
+    }
+};
