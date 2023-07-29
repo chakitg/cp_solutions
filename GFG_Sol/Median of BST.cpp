b20e8c94a1ba6bd9e14708d69a0da5de4b6df8ef
Chakit Gupta <76941922+chakitg@users.noreply.github.com>
2023-07-29 23:31:50 +0530

    Create Median of BST.cpp

---
+++ GFG_Sol/Median of BST.cpp
@@ -0,0 +1,76 @@
+Given a Binary Search Tree of size N, find the Median of its Node values.
+
+Example 1:
+
+Input:
+       6
+     /   \
+   3      8   
+ /  \    /  \
+1    4  7    9
+Output: 6
+Explanation: Inorder of Given BST will be:
+1, 3, 4, 6, 7, 8, 9. So, here median will 6.
+
+Example 2:
+
+Input:
+       6
+     /   \
+   3      8   
+ /   \    /   
+1    4   7   
+Output: 5
+Explanation:Inorder of Given BST will be:
+1, 3, 4, 6, 7, 8. So, here median will
+(4 + 6)/2 = 10/2 = 5.
+ 
+
+Your Task:
+You don't need to read input or print anything. Your task is to complete the function findMedian() which takes the root of the Binary Search Tree as input and returns the Median of Node values in the given BST.
+Median of the BST is:
+
+If number of nodes are even: then median = (N/2 th node + (N/2)+1 th node)/2
+If number of nodes are odd : then median = (N+1)/2th node.
+
+Expected Time Complexity: O(N).
+Expected Auxiliary Space: O(Height of the Tree).
+
+
+Constraints:
+1<=N<=10000
+
+
+
+
+/*
+Structure of the binary Search Tree is as
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+// your task is to complete the Function
+// Function should return median of the BST
+void solve(Node* ptr , vector<int>&nums){
+    if(ptr==nullptr)return;
+    solve(ptr->left ,nums);
+    nums.push_back(ptr->data);
+    solve(ptr->right, nums);
+}
+
+float findMedian(struct Node *root)
+{
+      float median = 0.00;
+      vector<int>nums;
+      solve(root, nums);
+      int size = nums.size();
+      median = (size&1)?nums[size/2]:(float)((float)nums[size/2-1]+(float)nums[(size/2)])/2;
+      return median;
+}
