5a5761fe6654bc98a7d6bf7b31869aff23235163
Chakit Gupta <76941922+chakitg@users.noreply.github.com>
2023-08-12 23:25:32 +0530

    Create 63. Unique Paths II.cpp

---
+++ Leetcode_Sol/63. Unique Paths II.cpp
@@ -0,0 +1,60 @@
+You are given an m x n integer array grid. There is a robot initially located at the top-left corner (i.e., grid[0][0]). The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either down or right at any point in time.
+
+An obstacle and space are marked as 1 or 0 respectively in grid. A path that the robot takes cannot include any square that is an obstacle.
+
+Return the number of possible unique paths that the robot can take to reach the bottom-right corner.
+
+The testcases are generated so that the answer will be less than or equal to 2 * 109.
+
+ 
+
+Example 1:
+
+
+Input: obstacleGrid = [[0,0,0],[0,1,0],[0,0,0]]
+Output: 2
+Explanation: There is one obstacle in the middle of the 3x3 grid above.
+There are two ways to reach the bottom-right corner:
+1. Right -> Right -> Down -> Down
+2. Down -> Down -> Right -> Right
+Example 2:
+
+
+Input: obstacleGrid = [[0,1],[0,0]]
+Output: 1
+ 
+
+Constraints:
+
+m == obstacleGrid.length
+n == obstacleGrid[i].length
+1 <= m, n <= 100
+obstacleGrid[i][j] is 0 or 1.
+
+
+
+class Solution {
+public:
+    int uniquePathsWithObstacles(std::vector<std::vector<int>>& obstacleGrid) {
+        if (obstacleGrid.empty() || obstacleGrid[0].empty() || obstacleGrid[0][0] == 1) {
+            return 0;
+        }
+        
+        int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size();
+        
+        std::vector<int> previous(n, 0);
+        std::vector<int> current(n, 0);
+        previous[0] = 1;
+        
+        for (int i = 0; i < m; i++) {
+            current[0] = obstacleGrid[i][0] == 1 ? 0 : previous[0];
+            for (int j = 1; j < n; j++) {
+                current[j] = obstacleGrid[i][j] == 1 ? 0 : current[j-1] + previous[j];
+            }
+            previous = current;
+        }
+        
+        return previous[n-1];
+    }
+};
