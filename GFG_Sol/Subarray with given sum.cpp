d5d39b046c0533a00086abb1d4981488ccbfd63d
Chakit Gupta <76941922+chakitg@users.noreply.github.com>
2023-08-19 23:21:09 +0530

    Create Subarray with given sum.cpp

---
+++ GFG_Sol/Subarray with given sum.cpp
@@ -0,0 +1,61 @@
+Given an unsorted array A of size N that contains only positive integers, find a continuous sub-array that adds to a given number S and return the left and right index(1-based indexing) of that subarray.
+
+In case of multiple subarrays, return the subarray indexes which come first on moving from left to right.
+
+Note:- You have to return an ArrayList consisting of two elements left and right. In case no such subarray exists return an array consisting of element -1.
+
+Example 1:
+
+Input:
+N = 5, S = 12
+A[] = {1,2,3,7,5}
+Output: 2 4
+Explanation: The sum of elements 
+from 2nd position to 4th position 
+is 12.
+Example 2:
+
+Input:
+N = 10, S = 15
+A[] = {1,2,3,4,5,6,7,8,9,10}
+Output: 1 5
+Explanation: The sum of elements 
+from 1st position to 5th position
+is 15.
+Your Task:
+You don't need to read input or print anything. The task is to complete the function subarraySum() which takes arr, N, and S as input parameters and returns an ArrayList containing the starting and ending positions of the first such occurring subarray from the left where sum equals to S. The two indexes in the array should be according to 1-based indexing. If no such subarray is found, return an array consisting of only one element that is -1.
+
+Expected Time Complexity: O(N)
+Expected Auxiliary Space: O(1)
+
+Constraints:
+1 <= N <= 105
+1 <= Ai <= 109
+0<= S <= 109
+
+
+
+class Solution
+{
+    public:
+    //Function to find a continuous sub-array which adds up to a given number.
+    vector<int> subarraySum(vector<int>arr, int n, long long s)
+    {
+        // Your code here
+        int l =0,r =0;
+        long long sum =0;
+        while(r<n){
+        sum += arr[r];
+        while(sum>s && l<r){
+            sum -= arr[l];
+            l++;
+        }
+
+        if(sum == s){
+            return {l+1,r+1};
+        }
+        r++;
+    }
+        return {-1};
+    }
+};
