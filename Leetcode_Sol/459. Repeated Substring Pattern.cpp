bd31da35625870a4596be4499c356c5a8aa660d7
Chakit Gupta <76941922+chakitg@users.noreply.github.com>
2023-08-21 23:33:55 +0530

    Create 459. Repeated Substring Pattern.cpp

---
+++ Leetcode_Sol/459. Repeated Substring Pattern.cpp
@@ -0,0 +1,33 @@
+Given a string s, check if it can be constructed by taking a substring of it and appending multiple copies of the substring together.
+
+ 
+
+Example 1:
+
+Input: s = "abab"
+Output: true
+Explanation: It is the substring "ab" twice.
+Example 2:
+
+Input: s = "aba"
+Output: false
+Example 3:
+
+Input: s = "abcabcabcabc"
+Output: true
+Explanation: It is the substring "abc" four times or the substring "abcabc" twice.
+ 
+
+Constraints:
+
+1 <= s.length <= 104
+s consists of lowercase English letters.
+
+
+
+class Solution {
+public:
+     bool repeatedSubstringPattern(string s) {
+        return (s + s).substr(1, 2*s.size()-2).find(s) != -1;
+    }
+};
