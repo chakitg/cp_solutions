e36e777e86102bee07fe2a40bbd1cf48b0474d2a
Chakit Gupta <76941922+chakitg@users.noreply.github.com>
2023-08-22 23:57:17 +0530

    Create 168. Excel Sheet Column Title.cpp

---
+++ Leetcode_Sol/168. Excel Sheet Column Title.cpp
@@ -0,0 +1,53 @@
+Given an integer columnNumber, return its corresponding column title as it appears in an Excel sheet.
+
+For example:
+
+A -> 1
+B -> 2
+C -> 3
+...
+Z -> 26
+AA -> 27
+AB -> 28 
+...
+ 
+
+Example 1:
+
+Input: columnNumber = 1
+Output: "A"
+Example 2:
+
+Input: columnNumber = 28
+Output: "AB"
+Example 3:
+
+Input: columnNumber = 701
+Output: "ZY"
+ 
+
+Constraints:
+
+1 <= columnNumber <= 231 - 1
+
+
+
+
+class Solution {
+public:
+    string convertToTitle(int columnNumber) {
+        string allAlphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        string answer = "";
+        int reminder;
+
+        while(columnNumber > 0) {
+            columnNumber -= 1;
+            reminder = columnNumber % 26;
+            answer.push_back(allAlphabets[reminder]);
+            columnNumber /= 26;
+        }
+
+        reverse(answer.begin(), answer.end());
+        return answer;
+    }
+};
