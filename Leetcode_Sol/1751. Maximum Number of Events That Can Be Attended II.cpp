f8a9ec54c5fc43aa652ed39025a55a0ed0ff04f5
Chakit Gupta <76941922+chakitg@users.noreply.github.com>
2023-08-23 00:01:10 +0530

    Update 1751. Maximum Number of Events That Can Be Attended II.cpp

--- Leetcode_Sol/1751. Maximum Number of Events That Can Be Attended II.cpp
+++ Leetcode_Sol/1751. Maximum Number of Events That Can Be Attended II.cpp
@@ -83,3 +83,35 @@ int helper(int i,vector<vector<int>>&events,int k, vector<vector<int>>&dp)
     return helper(0,events,k,dp);
   }
 };
+
+
+
+
+
+class Solution {
+public:
+
+int n;
+
+    int solve(int i , int k , vector<vector<int>> &a , vector<vector<int>> &dp) {
+        if(i >= n) {
+            return 0;
+        }
+        if(dp[i][k] != -1) return dp[i][k];
+        int ans = 0;
+        vector<int>e = {a[i][1]+1,INT_MIN,INT_MIN};
+        int id = lower_bound(a.begin(),a.end(),e) - a.begin();
+        if(k > 0) {
+            ans = max(ans , a[i][2] + solve(id,k-1,a,dp));
+        }
+        ans = max(ans,solve(i+1,k,a,dp));
+        return dp[i][k] = ans;
+    }
+
+    int maxValue(vector<vector<int>>& events, int k) {
+        sort(events.begin(),events.end());
+        n = (int)events.size();
+        vector<vector<int>>dp(n+2,vector<int>(k+2,-1));
+        return solve(0,k,events,dp);
+    }
+};
