da7ad51a5077df08a5c3261c63e9c69334b1c4fe
Chakit Gupta <76941922+chakitg@users.noreply.github.com>
2023-08-26 23:32:28 +0530

    Create 2302. Count Subarrays With Score Less Than K.cpp

---
+++ Leetcode_Sol/2302. Count Subarrays With Score Less Than K.cpp
@@ -0,0 +1,60 @@
+The score of an array is defined as the product of its sum and its length.
+
+For example, the score of [1, 2, 3, 4, 5] is (1 + 2 + 3 + 4 + 5) * 5 = 75.
+Given a positive integer array nums and an integer k, return the number of non-empty subarrays of nums whose score is strictly less than k.
+
+A subarray is a contiguous sequence of elements within an array.
+
+ 
+
+Example 1:
+
+Input: nums = [2,1,4,3,5], k = 10
+Output: 6
+Explanation:
+The 6 subarrays having scores less than 10 are:
+- [2] with score 2 * 1 = 2.
+- [1] with score 1 * 1 = 1.
+- [4] with score 4 * 1 = 4.
+- [3] with score 3 * 1 = 3. 
+- [5] with score 5 * 1 = 5.
+- [2,1] with score (2 + 1) * 2 = 6.
+Note that subarrays such as [1,4] and [4,3,5] are not considered because their scores are 10 and 36 respectively, while we need scores strictly less than 10.
+Example 2:
+
+Input: nums = [1,1,1], k = 5
+Output: 5
+Explanation:
+Every subarray except [1,1,1] has a score less than 5.
+[1,1,1] has a score (1 + 1 + 1) * 3 = 9, which is greater than 5.
+Thus, there are 5 subarrays having scores less than 5.
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+1 <= nums[i] <= 105
+1 <= k <= 1015
+
+
+class Solution {
+public:
+    long long countSubarrays(vector<int>& nums, long long k) {
+        int n = (int)nums.size();
+        long long ans = 0;
+        long long l = 0;
+        long long sum = 0;
+        for(long long r = 0 ; r < n ; r++) {
+            sum += nums[r];
+            while(sum * (r - l + 1) >= k) {
+                sum -= nums[l];
+                l++;
+            }
+            long long x = sum * (r - l + 1);
+            if(x < k) {
+                ans += max(0LL,r - l + 1);
+            }
+        }
+        return ans;
+    }
+};
