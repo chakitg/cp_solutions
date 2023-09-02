54077bc84e921ef4c556577e1e96e69d36153eeb
Chakit Gupta <76941922+chakitg@users.noreply.github.com>
2023-09-02 19:56:40 +0530

    Create Leaf under budget.cpp

---
+++ GFG_Sol/Leaf under budget.cpp
@@ -0,0 +1,75 @@
+Given a binary tree and a budget. Assume you are at the root of the tree(level 1), you need to maximise the count of leaf nodes you can visit in your budget if the cost of visiting a leaf node is equal to the level of that leaf node.
+
+Example 1:
+
+Input: 
+                  10
+                /    \
+               8      2
+             /      /   \
+            3      3     6
+                    \
+                     4
+and budget = 8
+Output: 2
+Explanation:
+Cost For visiting Leaf Node 3: 3
+Cost For visiting Leaf Node 4: 4
+Cost For visiting Leaf Node 6: 3
+In budget 8 one can visit Max 2 Leaf Nodes.
+Example 2:
+
+Input: 
+         1
+       /   \
+      2     3
+     / \   / \
+    4   5 6   7
+and budget = 5
+Output: 1
+Explanation: We can only visit either node 4 or 5.
+Your Task:
+
+You don't need to read input or print anything. Your task is to complete the function getCount() which takes root node of the tree and a integer denoting the budget as input parameters and returns an integer denoting the count of visited leaf nodes of the tree.
+
+Expected Time Complexity: O(N)
+Expected Auxiliary Space: O(N)
+
+Constraints:
+1<=N<=105
+1<=budget<=104
+
+
+
+
+
+class Solution
+{
+public:
+    int getCount(Node *root, int k)
+    {
+        int ans=0;
+        queue<pair<Node*,int> > q;
+        q.push(make_pair(root,1));
+        
+        while(!q.empty()){
+            Node* curr;
+            int level;
+            
+            tie(curr,level)=q.front();
+            q.pop();
+            
+            if(curr->left) q.push({curr->left,level+1});
+            if(curr->right) q.push({curr->right,level+1});
+            
+            if(!curr->left && !curr->right){
+                if(level<=k){
+                    k-=level;
+                    ans++;
+                }
+            }
+            
+        }
+        return ans;
+    }
+};
