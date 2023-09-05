2cfca5190bb257ef0ba84dde86c4a6757b3a37e8
Chakit Gupta <76941922+chakitg@users.noreply.github.com>
2023-09-05 23:57:29 +0530

    Create Print adjacency list.cpp

---
+++ GFG_Sol/Print adjacency list.cpp
@@ -0,0 +1,63 @@
+Given an undirected graph with V nodes and E edges, create and return an adjacency list of the graph. 0-based indexing is followed everywhere.
+
+Example 1:
+
+Input:
+V = 5, E = 7
+edges = {(0,1),(0,4),(4,1),(4,3),(1,3),(1,2),(3,2)}
+
+Output: 
+{{1,4}, 
+ {0,2,3,4}, 
+ {1,3},
+ {1,2,4},
+ {0,1,3}}
+Explanation:
+Node 0 is connected to 1 and 4.
+Node 1 is connected to 0,2,3 and 4.
+Node 2 is connected to 1 and 3.
+Node 3 is connected to 1,2 and 4.
+Node 4 is connected to 0,1 and 3.
+Example 2:
+
+Input:
+V = 4, E = 3
+edges = {(0,3),(0,2),(2,1)}
+
+
+Output: 
+{{2,3} 
+ {2}, 
+ {0,1} 
+ {0}}
+Explanation:
+Node 0 is connected to 2 and 3.
+Node 1 is only connected to 2.
+Node 2 is connected to 0 and 1.
+Node 3 is only connected to 0.
+Your task:
+You don't need to read input or print anything. Your task is to complete the function printGraph() which takes the integer V denoting the number of vertices and edges as input parameters and returns the list of list denoting the adjacency list.
+
+Expected Time Complexity: O(V + E)
+Expected Auxiliary Space: O(V + E)
+
+Constraints:
+1 ≤ V, E ≤ 105
+
+
+
+class Solution {
+  public:
+    // Function to return the adjacency list for each vertex.
+    vector<vector<int>> printGraph(int V, vector<pair<int,int>>edges) {
+        // Code here
+        vector<vector<int>> ans(V);
+        for(int i = 0; i < edges.size(); i++){
+            int a=edges[i].first;
+            int b=edges[i].second;
+            ans[a].push_back(b);
+            ans[b].push_back(a);
+        }
+        return ans;
+    }
+};
