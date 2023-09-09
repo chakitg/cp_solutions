f0a285b057fd6236ce40b37e8aa303aae830b2f1
Chakit Gupta <76941922+chakitg@users.noreply.github.com>
2023-09-09 23:10:15 +0530

    Create Kth largest element in BST.cpp

---
+++ GFG_Sol/Kth largest element in BST.cpp
@@ -0,0 +1,51 @@
+Given a Binary Search Tree. Your task is to complete the function which will return the Kth largest element without doing any modification in Binary Search Tree.
+
+Example 1:
+
+Input:
+      4
+    /   \
+   2     9
+k = 2 
+Output: 4
+Example 2:
+
+Input:
+       9
+        \ 
+          10
+K = 1
+Output: 10
+Your Task:
+You don't need to read input or print anything. Your task is to complete the function kthLargest() which takes the root of the BST and an integer K as inputs and returns the Kth largest element in the given BST.
+
+Expected Time Complexity: O(N).
+Expected Auxiliary Space: O(H) where H is max recursion stack of height H at a given time.
+
+Constraints:
+1 <= N <= 105
+1 <= K <= N
+
+
+
+
+class Solution
+{
+public:
+    void modifiedInorder(Node* root, int &k, int &ans){
+        if(!root)
+            return;
+        
+        modifiedInorder(root->right, k, ans);
+        --k;
+        if(!k)  ans = root->data;
+        modifiedInorder(root->left, k,  ans);
+    }
+    
+    int kthLargest(Node *root, int k)
+    {
+        int ans;
+        modifiedInorder(root, k, ans);
+        return ans;
+    }
+};
