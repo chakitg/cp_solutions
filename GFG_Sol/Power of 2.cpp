89b87d345bb57af26e8cd08bd5850614c10cfec8
Chakit Gupta <76941922+chakitg@users.noreply.github.com>
2023-09-18 23:38:20 +0530

    Create Power of 2.cpp

---
+++ GFG_Sol/Power of 2.cpp
@@ -0,0 +1,45 @@
+Given a non-negative integer N. The task is to check if N is a power of 2. More formally, check if N can be expressed as 2x for some integer x.
+
+Example 1:
+
+Input: 
+N = 8
+Output: 
+YES
+Explanation:
+8 is equal to 2 raised to 3 (23 = 8).
+Example 2:
+
+Input: 
+N = 98
+Output: 
+NO
+Explanation: 
+98 cannot be obtained by any power of 2.
+Your Task:Your task is to complete the function isPowerofTwo() which takes n as a parameter and returns true or false by checking if the given number can be represented as a power of two or not.
+
+Expected Time Complexity:O(log N).
+Expected Auxiliary Space:O(1).
+
+Constraints:
+0 ≤ N ≤1018
+
+
+
+
+  class Solution{
+    public:
+    // Function to check if given number n is a power of two.
+    bool isPowerofTwo(long long n){
+        
+        // Your code here    
+        if(n==0)
+            return false;
+        // Your code here    
+        if(n&(n-1))
+            return false;
+        else 
+            return true;  
+        
+    }
+};
