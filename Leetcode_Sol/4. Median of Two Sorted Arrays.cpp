84dd876bcd9ed796e29188d47d41cb3e1604a4c4
Chakit Gupta <76941922+chakitg@users.noreply.github.com>
2023-09-21 21:14:18 +0530

    Create 4. Median of Two Sorted Arrays.cpp

---
+++ Leetcode_Sol/4. Median of Two Sorted Arrays.cpp
@@ -0,0 +1,58 @@
+Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+
+The overall run time complexity should be O(log (m+n)).
+
+ 
+
+Example 1:
+
+Input: nums1 = [1,3], nums2 = [2]
+Output: 2.00000
+Explanation: merged array = [1,2,3] and median is 2.
+Example 2:
+
+Input: nums1 = [1,2], nums2 = [3,4]
+Output: 2.50000
+Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
+ 
+
+Constraints:
+
+nums1.length == m
+nums2.length == n
+0 <= m <= 1000
+0 <= n <= 1000
+1 <= m + n <= 2000
+-106 <= nums1[i], nums2[i] <= 106
+
+
+
+  // Brute Force:
+               // 1.Merge Both Array
+              // 2.Sort them
+             // 3.Find Median
+            // TIME COMPLEXITY: O(n)+O(nlogn)+O(n)
+            // SPACE COMPLEXITY: O(1)
+ 
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+       // Initialization some neccessary variables
+        vector<int>v;
+        
+        // store the array in the new array
+        for(auto num:nums1)   // O(n1)
+            v.push_back(num);
+        
+        for(auto num:nums2)  // O(n2)
+            v.push_back(num);
+        
+        // Sort the array to find the median
+        sort(v.begin(),v.end());  // O(nlogn)
+        
+        // Find the median and Return it
+        int n=v.size();  // O(n)
+        
+        return n%2?v[n/2]:(v[n/2-1]+v[n/2])/2.0;
+    }
+};
