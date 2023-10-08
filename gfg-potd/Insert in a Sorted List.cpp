7888b925ca28c8427ed8d920df02094e0bbb3929
Chakit Gupta <76941922+chakitg@users.noreply.github.com>
2023-10-08 22:22:28 +0530

    Create Insert in a Sorted List.cpp

---
+++ gfg-potd/Insert in a Sorted List.cpp
@@ -0,0 +1,48 @@
+Given a linked list sorted in ascending order and an integer called data, insert data in the linked list such that the list remains sorted.
+
+Example 1:
+
+Input:
+LinkedList: 25->36->47->58->69->80
+data: 19
+Output: 
+19 25 36 47 58 69 80
+Explanation:
+After inserting 19 the sorted linked list will look like the one in the output.
+Example 2:
+
+Input:
+LinkedList: 50->100
+data: 75
+Output: 
+50 75 100
+Explanation:
+After inserting 75 the sorted linked list will look like the one in the output.
+Your Task:
+The task is to complete the function sortedInsert() which should insert the element in sorted Linked List and return the head of the linked list
+
+Expected Time Complexity: O(N).
+Expected Auxiliary Space: O(1).
+
+Constraints:
+1 <= N <= 104
+-99999 <= A[i] <= 99999, for each valid i
+
+
+class Solution {
+public:
+    Node *sortedInsert(struct Node* head, int data) {
+        struct Node * newNode =  new Node(data);
+        if(head->data >= data){
+            newNode->next = head;
+            return newNode;
+        }
+        struct Node * itr = head;
+        while(itr->next && itr->next->data < data){
+            itr = itr->next;
+        }
+        newNode->next = itr->next;
+        itr->next = newNode;
+        return head;
+    }
+};
