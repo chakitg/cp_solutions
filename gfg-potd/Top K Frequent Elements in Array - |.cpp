0d2784f27b00b0c4e386c25755b8130b6288e148
Chakit Gupta <76941922+chakitg@users.noreply.github.com>
2023-11-05 22:43:18 +0530

    Create Top K Frequent Elements in Array - |.cpp

---
+++ gfg-potd/Top K Frequent Elements in Array - |.cpp
@@ -0,0 +1,56 @@
+Given a non-empty array nums[] of integers of length N, find the top k elements which have the highest frequency in the array. If two numbers have same frequencies, then the larger number should be given more preference.
+
+Example 1:
+
+Input:
+N = 6
+nums = {1,1,1,2,2,3}
+k = 2
+Output: {1, 2}
+Example 2:
+
+Input:
+N = 8
+nums = {1,1,2,2,3,3,3,4}
+k = 2
+Output: {3, 2}
+Explanation: Elements 1 and 2 have the
+same frequency ie. 2. Therefore, in this
+case, the answer includes the element 2
+as 2 > 1.
+User Task:
+You don't have to read or print anything. You will have to complete the function topK() that takes an array nums[] and integer k as input and returns a list of top k most frequent elements. If any two elements have same frequencies, then give more preference to the larger elemet.
+
+Expected Time Complexity : O(NlogN)
+Expected Auxilliary Space : O(N)
+
+Constraints:
+1 <= N <= 105
+1<= nums[i] <=105
+1 <= k <= N
+
+
+
+
+class Solution {
+public:
+    vector<int> topK(vector<int>& nums, int k) {
+        unordered_map<int, int> mp;
+        for (auto i : nums)
+            ++mp[i];
+        
+        priority_queue<pair<int, int>> pq;
+        
+        for (auto itr : mp) {
+            pq.push({itr.second, itr.first});
+        }
+        
+        vector<int> out;
+        while (!pq.empty() && k--) {
+            out.push_back(pq.top().second);
+            pq.pop();
+        }
+        
+        return out;
+    }
+};
