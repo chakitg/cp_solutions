1f40218fb64f55e90bad6ca39a5fbe9bdcda8d68
Chakit Gupta <76941922+chakitg@users.noreply.github.com>
2023-11-13 19:41:10 +0530

    Update 2785. Sort Vowels in a String.cpp

--- leetcode-potd/2785. Sort Vowels in a String.cpp
+++ leetcode-potd/2785. Sort Vowels in a String.cpp
@@ -50,3 +50,35 @@ public:
         return s;
     }
 };
+
+________________________________________________________________________________________________________________
+
+class Solution {
+public:
+    string sortVowels(string s) {
+        unordered_map<char, int> hash;
+        hash.insert({'A', 1});
+        hash.insert({'E', 1});
+        hash.insert({'I', 1});
+        hash.insert({'O', 1});
+        hash.insert({'U', 1});
+        hash.insert({'a', 1});
+        hash.insert({'e', 1});
+        hash.insert({'i', 1});
+        hash.insert({'o', 1});
+        hash.insert({'u', 1});
+        vector<int> idsave;
+        vector<char> vowsave;
+        for(int i=0; i<s.size(); i++) {
+            if(hash.find(s[i])!=hash.end()) {
+                idsave.push_back(i);
+                vowsave.push_back(s[i]);
+            }
+        }
+        sort(vowsave.begin(), vowsave.end());
+        for(int i=0; i<idsave.size(); i++) {
+            s[idsave[i]] = vowsave[i];
+        }
+        return s;
+    }
+};
