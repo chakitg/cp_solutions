9ab3241873f35ebb71f64200bdd99abdb165116f
Chakit Gupta <76941922+chakitg@users.noreply.github.com>
2023-11-19 15:44:18 +0530

    Create Intersection of two sorted Linked lists.cpp

---
+++ gfg-potd/Intersection of two sorted Linked lists.cpp
@@ -0,0 +1,57 @@
+Given two linked lists sorted in increasing order, create a new linked list representing the intersection of the two linked lists. The new linked list should be made with without changing the original lists.
+
+Note: The elements of the linked list are not necessarily distinct.
+
+Example 1:
+
+Input:
+LinkedList1 = 1->2->3->4->6
+LinkedList2 = 2->4->6->8
+Output: 2 4 6
+Explanation: For the given two
+linked list, 2, 4 and 6 are the elements
+in the intersection.
+Example 2:
+
+Input:
+LinkedList1 = 10->20->40->50
+LinkedList2 = 15->40
+Output: 40
+Your Task:
+You don't have to take any input of print anything. Your task is to complete the function findIntersection(), which will take head of both of the linked lists as input and should find the intersection of two linked list and add all the elements in intersection to the third linked list and return the head of the third linked list.
+
+Expected Time Complexity : O(n+m)
+Expected Auxilliary Space : O(n+m)
+Note: n, m are the size of the respective linked lists.
+
+Constraints:
+1 <= size of linked lists <= 5000
+1 <= Data in linked list nodes <= 104
+
+
+
+
+class Solution {
+public:
+    Node* findIntersection(Node* head1, Node* head2)
+    {
+        Node* head = new Node(0);
+        Node* tail = head;
+        
+        while (head1 && head2) {
+            if (head1->data == head2->data) {
+                tail->next = head1;
+                tail = head1;
+                head1 = head1->next;
+                head2 = head2->next;
+            } else if (head1->data < head2->data)
+                head1 = head1->next;
+            else
+                head2 = head2->next;
+        }
+        head = head->next;
+        tail->next = NULL; 
+        
+        return head;
+    }
+};
