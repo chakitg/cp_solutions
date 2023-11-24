55f65d83dea47078974abdb91a9de2137a479663
Chakit Gupta <76941922+chakitg@users.noreply.github.com>
2023-11-24 23:14:29 +0530

    Create Pascal Triangle.cpp

---
+++ gfg-potd/Pascal Triangle.cpp
@@ -0,0 +1,48 @@
+Given a positive integer N, return the Nth row of pascal's triangle.
+Pascal's triangle is a triangular array of the binomial coefficients formed by summing up the elements of previous row.
+The elements can be large so return it modulo 109 + 7.
+
+File:PascalTriangleAnimated2.gif
+
+Example 1:
+
+Input:
+N = 4
+Output: 
+1 3 3 1
+Explanation: 
+4th row of pascal's triangle is 1 3 3 1.
+Example 2:
+
+Input:
+N = 5
+Output: 
+1 4 6 4 1
+Explanation: 
+5th row of pascal's triangle is 1 4 6 4 1.
+Your Task:
+Complete the function nthRowOfPascalTriangle() which takes n, as input parameters and returns an array representing the answer. You don't to print answer or take inputs.
+
+Expected Time Complexity: O(N2)
+Expected Auxiliary Space: O(N2)
+
+Constraints:
+1 ≤ N ≤ 103
+
+
+
+class Solution {
+public:
+    int mod = 1e9 + 7;
+
+    vector<long long> nthRowOfPascalTriangle(int n) {
+        vector<long long> out(n, 1), prev(n, 1);
+        for (int i = 1; i < n; ++i) {
+            for (int j = 1; j < i; ++j) {
+                out[j] = (prev[j] + prev[j - 1]) % mod;
+            }
+            prev = out;
+        }
+        return out;
+    }
+};
