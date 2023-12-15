6b73217912f0a230da4b3d6f15fa7e5fae03f8c6
Chakit Gupta <76941922+chakitg@users.noreply.github.com>
2023-12-15 23:13:07 +0530

    Create Reach the Nth point.cpp

---
+++ gfg-potd/Reach the Nth point.cpp
@@ -0,0 +1,48 @@
+There are N points on the road, you can step ahead by 1 or 2 . If you start from a point 0, and can only move from point i to point i+1 after taking a step of length one, find the number of ways you can reach at point N. 
+
+Example 1:
+
+Input: 
+N = 4
+Output: 
+5
+Explanation: Three ways to reach at 4th
+point. They are {1, 1, 1, 1}, {1, 1, 2},
+{1, 2, 1} {2, 1, 1}, {2, 2}.
+
+Example 2:
+
+Input: N = 5
+Output: 8
+Explanation: Three ways to reach at 5th
+point. They are {1, 1, 1, 1, 1},
+{1, 1, 1, 2}, {1, 1, 2, 1}, {1, 2, 1, 1},
+{2, 1, 1, 1}{1, 2, 2}, {2, 1, 2}, {2, 2, 1}
+Your Task:
+You don't need to read or print anyhting. Your task is to complete the function nthPoint() which takes N as input parameter and returns the total number of ways modulo 109 + 7 to reach at Nth point.
+
+Expected Time Complexity: O(N)
+Expected Space Complexity: O(N)
+
+Constraints:
+1 ≤ N ≤ 105
+
+
+
+
+
+class Solution
+{
+	public:
+	int nthPoint(int n)
+	{
+	    long long a=1, b=1, mod=1e9+7;
+	    for(int i=1;i<n;i++)
+	    {
+	        long long c=(a+b)%mod;
+	        a=b;
+	        b=c;
+	    }
+	    return b;
+	}
+};
