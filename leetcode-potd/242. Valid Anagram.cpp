672370065486e1ee823c995ac91c7b477d84cbab
Chakit Gupta <76941922+chakitg@users.noreply.github.com>
2023-12-16 21:45:10 +0530

    Create 242. Valid Anagram.cpp

---
+++ leetcode-potd/242. Valid Anagram.cpp
@@ -0,0 +1,32 @@
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+ 
+
+Example 1:
+
+Input: s = "anagram", t = "nagaram"
+Output: true
+Example 2:
+
+Input: s = "rat", t = "car"
+Output: false
+ 
+
+Constraints:
+
+1 <= s.length, t.length <= 5 * 104
+s and t consist of lowercase English letters.
+
+
+
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        return s==t;
+    }
+};
