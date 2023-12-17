a328c87415c112f27e636341d4ef39a3cf0961ca
Chakit Gupta <76941922+chakitg@users.noreply.github.com>
2023-12-17 23:06:01 +0530

    Create Max Sum without Adjacents.cpp

---
+++ gfg-potd/Max Sum without Adjacents.cpp
@@ -0,0 +1,50 @@
+Given an array Arr of size N containing positive integers. Find the maximum sum of a any possible subsequence such that no two numbers in the subsequence should be adjacent in Arr.
+
+Example 1:
+
+Input:
+N = 6
+Arr[] = {5, 5, 10, 100, 10, 5}
+Output: 110
+Explanation: If you take indices 0, 3
+and 5, then Arr[0]+Arr[3]+Arr[5] =
+5+100+5 = 110.
+Example 2:
+
+Input:
+N = 4
+Arr[] = {3, 2, 7, 10}
+Output: 13
+Explanation: 3 and 10 forms a non
+continuous subsequence with maximum
+sum.
+Your Task:
+You don't need to read input or print anything. Your task is to complete the function findMaxSum() which takes the array of integers Arr and N as parameters and returns an the maximum sum possible of any valid subsequence.
+
+Expected Time Complexity: O(N)
+Expected Auxiliary Space: O(1)
+
+Constraints:
+1 ≤ N ≤ 105
+1 ≤ Arri ≤ 105
+
+
+
+
+
+class Solution {
+public:
+    int findMaxSum(int *arr, int n) {
+        int lastPrev = 0;
+        int prev = arr[0];
+        int curr = 0;
+
+        for (int i = 1; i < n; ++i) {
+            curr = max(prev, arr[i] + lastPrev);
+            lastPrev = prev;
+            prev = curr;
+        }
+
+        return max(lastPrev, prev);
+    }
+};
