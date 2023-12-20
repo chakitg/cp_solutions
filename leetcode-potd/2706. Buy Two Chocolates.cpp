72b9be92e9dafceebc5f6ce242f416e14bd7fe40
Chakit Gupta <76941922+chakitg@users.noreply.github.com>
2023-12-20 21:21:07 +0530

    Update 2706. Buy Two Chocolates.cpp

--- leetcode-potd/2706. Buy Two Chocolates.cpp
+++ leetcode-potd/2706. Buy Two Chocolates.cpp
@@ -36,3 +36,17 @@ public:
         }
     }
 };
+
+
+________________________________________________________
+
+
+class Solution {
+public:
+    int buyChoco(vector<int>& prices, int money) {
+        sort(prices.begin(),prices.end());
+        if(prices[0]+prices[1]<=money){
+            return money-(prices[1]+prices[0]);
+        }return money;
+    }
+};
