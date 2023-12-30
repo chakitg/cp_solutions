f96e863dcb787b9802fa0bac4700f4ce7d63616e
Chakit Gupta <76941922+chakitg@users.noreply.github.com>
2023-12-30 23:19:07 +0530

    Update 1897. Redistribute Characters to Make All Strings Equal.cpp

--- leetcode-potd/1897. Redistribute Characters to Make All Strings Equal.cpp
+++ leetcode-potd/1897. Redistribute Characters to Make All Strings Equal.cpp
@@ -48,3 +48,20 @@ public:
         return 1;
     }
 };
+
+
+
+
+
+
+
+
+class Solution:
+    def makeEqual(self, words: List[str]) -> bool:
+        n=len(words)
+        ans=''.join(words)
+        res=set(ans)
+        for i in res:
+            if ans.count(i)%n!=0:
+                return False
+        return True   
