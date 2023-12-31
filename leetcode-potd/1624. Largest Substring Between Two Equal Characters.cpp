b67ce597c9ac88bafe88a2d74b1b812837c1bfea
Chakit Gupta <76941922+chakitg@users.noreply.github.com>
2023-12-31 19:05:58 +0530

    Update 1624. Largest Substring Between Two Equal Characters.cpp

--- leetcode-potd/1624. Largest Substring Between Two Equal Characters.cpp
+++ leetcode-potd/1624. Largest Substring Between Two Equal Characters.cpp
@@ -25,3 +25,12 @@ Constraints:
 
 1 <= s.length <= 300
 s contains only lowercase English letters.
+
+
+
+ int maxLengthBetweenEqualCharacters(string s) {
+    int last[26] = {0}, ans = -1;
+    for(int i = 0; i < s.size(); ++i) last[s[i] - 'a'] = i;
+    for(int i = 0; i < s.size(); ++i) ans = max(ans, last[s[i] - 'a'] - i - 1);
+    return ans;
+}
