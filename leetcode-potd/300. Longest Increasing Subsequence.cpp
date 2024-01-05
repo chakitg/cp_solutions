17b30aacc3ec8f52eede3d9d720bf321ff87e2c3
Chakit Gupta <76941922+chakitg@users.noreply.github.com>
2024-01-05 21:40:40 +0530

    Update 300. Longest Increasing Subsequence.cpp

--- leetcode-potd/300. Longest Increasing Subsequence.cpp
+++ leetcode-potd/300. Longest Increasing Subsequence.cpp
@@ -44,3 +44,30 @@ public:
         return *max_element(ans.begin(),ans.end());
     }
 };
+
+
+_______________________________________________________________
+
+// TC = O(N * log N) - N binary searches of O(log N) each
+// SC = O(N)
+
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        vector<int> temp;
+        temp.push_back(nums[0]);
+        int l = 1; // len = 1 as we pushed nums[0]
+        // LOGIC : Create a temp array that adds elements if greater than the last element, and if not, overwrites smallest greater element. NOTE : temp does not store the longest increasing subsequence, we are just utilizing the length of temp to get our answer.
+        for(int i=1;i<nums.size();i++){
+            if(temp.back() < nums[i]){ // If nums[i] is greater, just add it at the back
+                temp.push_back(nums[i]);
+                l++;
+            }
+            else{ // If not, just replace the smallest element greater than nums[i] with nums[i]
+                int ind = lower_bound(temp.begin(),temp.end(),nums[i]) - temp.begin(); // inbuilt c++ function to perform binary search O(log N) to search for the third parameter in the array or element just greater than it. Do - temp.begin() to get an integer index value
+                temp[ind] = nums[i];
+            }
+        }
+        return l;
+    }
+};
