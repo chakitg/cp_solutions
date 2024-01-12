373e8e8850841a43aedf633983788737803f1888
Chakit Gupta <76941922+chakitg@users.noreply.github.com>
2024-01-12 23:45:43 +0530

    Update 1704. Determine if String Halves Are Alike.cpp

--- leetcode-potd/1704. Determine if String Halves Are Alike.cpp
+++ leetcode-potd/1704. Determine if String Halves Are Alike.cpp
@@ -52,3 +52,26 @@ public:
         return 0;
     }
 };
+
+
+
+// ----------------------------------------------------------------------------------
+
+
+class Solution {
+public:
+    bool halvesAreAlike(string s) {
+        int countLeftVowel = 0, countRightVowel = 0;
+        for (int i = 0; i < s.length()/2; i++) {
+            if (s[i] == 'a' || s[i] == 'A' || s[i] == 'e' || s[i] == 'E' || s[i] == 'i' || s[i] == 'I' || s[i] == 'o' || s[i] == 'O' || s[i] == 'u' || s[i] == 'U')
+            countLeftVowel++;
+        }
+        for (int i = s.length()/2; i < s.length(); i++) {
+            if (s[i] == 'a' || s[i] == 'A' || s[i] == 'e' || s[i] == 'E' || s[i] == 'i' || s[i] == 'I' || s[i] == 'o' || s[i] == 'O' || s[i] == 'u' || s[i] == 'U')
+            countRightVowel++;
+        }
+        if (countLeftVowel == countRightVowel)
+            return true;
+        return false;
+    }
+};
