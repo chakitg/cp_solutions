e2e80214e6099967d02831b1c063a7bfbb015ca7
Chakit Gupta <76941922+chakitg@users.noreply.github.com>
2024-01-13 23:25:26 +0530

    Create Insertion Sort for Singly Linked List.cpp

---
+++ gfg-potd/Insertion Sort for Singly Linked List.cpp
@@ -0,0 +1,58 @@
+Given a singly linked list, sort the list (in ascending order) using insertion sort algorithm.
+
+Example 1:
+
+Input:
+N = 10
+Linked List = 30->23->28->30->11->14->
+              19->16->21->25 
+Output : 
+11 14 16 19 21 23 25 28 30 30 
+Explanation :
+The resultant linked list is sorted.
+Example 2:
+
+Input : 
+N = 7
+Linked List=19->20->16->24->12->29->30 
+Output : 
+12 16 19 20 24 29 30 
+Explanation : 
+The resultant linked list is sorted.
+Your task:
+You don't need to read input or print anything. Your task is to complete the function insertionSort() which takes the head of the linked list, sorts the list using insertion sort algorithm and returns the head of the sorted linked list.
+ 
+Expected Time Complexity : O(n2)
+Expected Auxiliary Space : O(1)
+ 
+Constraints:
+0 <= n <= 5*103
+
+
+
+
+class Solution
+{
+    public:
+    Node* insertionSort(struct Node* head_ref)
+    {
+        vector<int>ans;
+        Node* temp=head_ref;
+        while (temp!=nullptr)
+        {
+            ans.push_back(temp->data);
+            temp=temp->next;
+        }
+        sort(ans.begin(), ans.end());
+        temp=head_ref;
+        int i=0;
+        while (temp!=nullptr)
+        {
+            temp->data=ans[i];
+            i++;
+            temp=temp->next;
+        }
+        return head_ref;
+    }
+    
+};
