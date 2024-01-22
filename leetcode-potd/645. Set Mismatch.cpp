9c52959560949865b6b585d0b011580b5470aa51
Chakit Gupta <76941922+chakitg@users.noreply.github.com>
2024-01-22 23:08:17 +0530

    Update 645. Set Mismatch.cpp

--- leetcode-potd/645. Set Mismatch.cpp
+++ leetcode-potd/645. Set Mismatch.cpp
@@ -47,3 +47,32 @@ public:
         return ans;
     }
 };
+
+
+
+____________________________________________________
+
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        int n = nums.size();
+        int sum = (n * (n + 1))/2;
+        long long sqr_sum = 0;
+        for(int i = 1; i <= n; i++) sqr_sum += i * i;
+
+        int arr_sum = 0;
+        long long arr_sqr_sum = 0;
+
+        for(int i = 0; i < n; i++){
+            arr_sum += nums[i];
+            arr_sqr_sum += (nums[i] * nums[i]);
+        }
+
+        int sum_diff = arr_sum - sum;
+        int sqr_sum_diff = arr_sqr_sum - sqr_sum;
+
+        int missing = (sqr_sum_diff - (sum_diff * sum_diff)) / (2 * sum_diff);
+        int duplicate = sum_diff + (missing);
+        return {duplicate , missing};
+    }
+};
