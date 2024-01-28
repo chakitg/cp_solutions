a1151022762bd0f4539b76be7e75ea9cbfd55435
Chakit Gupta <76941922+chakitg@users.noreply.github.com>
2024-01-28 23:33:47 +0530

    Create 1074. Number of Submatrices That Sum to Target.cpp

---
+++ gfg-potd/1074. Number of Submatrices That Sum to Target.cpp
@@ -0,0 +1,68 @@
+Given a matrix and a target, return the number of non-empty submatrices that sum to target.
+
+A submatrix x1, y1, x2, y2 is the set of all cells matrix[x][y] with x1 <= x <= x2 and y1 <= y <= y2.
+
+Two submatrices (x1, y1, x2, y2) and (x1', y1', x2', y2') are different if they have some coordinate that is different: for example, if x1 != x1'.
+
+ 
+
+Example 1:
+
+
+Input: matrix = [[0,1,0],[1,1,1],[0,1,0]], target = 0
+Output: 4
+Explanation: The four 1x1 submatrices that only contain 0.
+Example 2:
+
+Input: matrix = [[1,-1],[-1,1]], target = 0
+Output: 5
+Explanation: The two 1x2 submatrices, plus the two 2x1 submatrices, plus the 2x2 submatrix.
+Example 3:
+
+Input: matrix = [[904]], target = 0
+Output: 0
+ 
+
+Constraints:
+
+1 <= matrix.length <= 100
+1 <= matrix[0].length <= 100
+-1000 <= matrix[i] <= 1000
+-10^8 <= target <= 10^8
+
+
+
+
+
+class Solution {
+public:
+    int getCount(vector<int> &prefix, int target){
+        unordered_map<int,int> m;
+        int sum = 0;
+        int ans = 0;
+        m[0]=1;
+        for(auto i : prefix){
+            sum += i;
+            if(m[sum-target] > 0){
+                ans += m[sum-target];
+            }
+            m[sum]++;
+        }
+        return ans;
+    }
+    int numSubmatrixSumTarget(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int ans = 0;
+        for(int i=0;i<m; i++){
+            vector<int> prefix(n, 0);
+            for(int j=i; j<m; j++){
+                for(int k=0;k<n;k++){
+                    prefix[k] += matrix[j][k];
+                }
+                ans += getCount(prefix, target);
+            }
+        }
+        return ans;
+    }
+};
