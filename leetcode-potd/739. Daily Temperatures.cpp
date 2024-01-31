9120ebf3316f402f6520e976c49151c9bca7e9ee
Chakit Gupta <76941922+chakitg@users.noreply.github.com>
2024-01-31 23:58:39 +0530

    Create 739. Daily Temperatures.cpp

---
+++ leetcode-potd/739. Daily Temperatures.cpp
@@ -0,0 +1,53 @@
+Given an array of integers temperatures represents the daily temperatures, return an array answer such that answer[i] is the number of days you have to wait after the ith day to get a warmer temperature. If there is no future day for which this is possible, keep answer[i] == 0 instead.
+
+ 
+
+Example 1:
+
+Input: temperatures = [73,74,75,71,69,72,76,73]
+Output: [1,1,4,2,1,1,0,0]
+Example 2:
+
+Input: temperatures = [30,40,50,60]
+Output: [1,1,1,0]
+Example 3:
+
+Input: temperatures = [30,60,90]
+Output: [1,1,0]
+ 
+
+Constraints:
+
+1 <= temperatures.length <= 105
+30 <= temperatures[i] <= 100
+
+
+
+
+
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& t) {
+        stack<pair<int,int>>st;
+        int n=t.size();
+        st.push({t[n-1],n-1});
+        
+        vector<int>ans(n,0);
+
+        for(int i=n-2;i>=0;i--)
+        {
+            while(!st.empty() && st.top().first <= t[i])
+            {
+                st.pop();
+            }
+            if(!st.empty())
+            {
+                ans[i] = st.top().second - i;
+            }
+            st.push({t[i],i});
+        }
+
+        return ans;
+        
+    }
+};
