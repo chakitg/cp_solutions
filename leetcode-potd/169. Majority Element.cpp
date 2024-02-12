11f1c1e782fd7669daa5b134c7b0f11555724447
Chakit Gupta <76941922+chakitg@users.noreply.github.com>
2024-02-12 22:46:18 +0530

    Update 169. Majority Element.cpp

--- leetcode-potd/169. Majority Element.cpp
+++ leetcode-potd/169. Majority Element.cpp
@@ -38,3 +38,13 @@ public:
       return 0;
     }
 };
+
+// ---------------------------------------------------
+
+class Solution {
+public:
+    int majorityElement(std::vector<int>& nums) {
+        std::sort(nums.begin(), nums.end());
+        return nums[nums.size() / 2];
+    }
+};
