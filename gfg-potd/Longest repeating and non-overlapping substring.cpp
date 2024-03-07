305025d01753e2f67406d8bb7eec034fe12febd3
Chakit Gupta <76941922+chakitg@users.noreply.github.com>
2024-03-07 23:18:57 +0530

    Create Longest repeating and non-overlapping substring.cpp

---
+++ gfg-potd/Longest repeating and non-overlapping substring.cpp
@@ -0,0 +1,54 @@
+Given a string s of length n, find the longest repeating non-overlapping substring in it. In other words find 2 identical substrings of maximum length which do not overlap. Return the longest non-overlapping substring. Return "-1" if no such string exists.
+
+Note: Multiple Answers are possible but you have to return the substring whose first occurrence is earlier.
+
+For Example: "abhihiab", here both "ab" and "hi" are possible answers. But you will have to return "ab" because it's first occurrence appears before the first occurrence of "hi".
+
+Example 1:
+
+Input:
+n = 9
+s = "acdcdacdc"
+Output:
+"acdc"
+Explanation:
+The string "acdc" is the longest Substring of s which is repeating but not overlapping.
+Example 2:
+
+Input:
+n = 7
+s = "heheheh"
+Output:
+"heh"
+Explanation:
+The string "heh" is the longest Substring of s which is repeating but not overlapping.
+Your Task:
+You don't need to read input or print anything. Your task is to complete the function longestSubstring() which takes an Integer n and a string s as input and returns the answer.
+
+Expected Time Complexity: O(n2)
+Expected Auxiliary Space: O(n2)
+
+Constraints:
+1 <= n <= 103
+
+
+
+
+class Solution {
+public:
+    string longestSubstring(string s, int n) {
+        int nax = 0, i = 0, j = 0;
+        string out = "-1";
+    
+        for( ; i < n && j < n; ++j) {
+            string str = s.substr(i, j - i + 1);
+    
+            if (nax < str.size() && s.find(str, j + 1) != string::npos) {
+                nax = str.size();
+                out = str;
+            } else 
+                ++i;
+        }
+        return out;
+    }
+};
