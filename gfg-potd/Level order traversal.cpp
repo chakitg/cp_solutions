5d3485eaf0d548292dd50e02c463344951d1ddb9
Chakit Gupta <76941922+chakitg@users.noreply.github.com>
2024-03-18 22:47:03 +0530

    Create Level order traversal.cpp

---
+++ gfg-potd/Level order traversal.cpp
@@ -0,0 +1,60 @@
+Given a root of a binary tree with n nodes, find its level order traversal.
+Level order traversal of a tree is breadth-first traversal for the tree.
+
+Example 1:
+
+Input:
+    1
+  /   \ 
+ 3     2
+Output:
+1 3 2
+Example 2:
+
+Input:
+        10
+     /      \
+    20       30
+  /   \
+ 40   60
+Output:
+10 20 30 40 60
+Your Task:
+You don't have to take any input. Complete the function levelOrder() that takes the root node as input parameter and returns a list of integers containing the level order traversal of the given Binary Tree.
+
+Expected Time Complexity: O(n)
+Expected Auxiliary Space: O(n)
+
+Constraints:
+1 ≤ n ≤ 105
+0 ≤ Data of a node ≤ 109
+
+
+
+
+
+class Solution {
+public:
+    vector<int> levelOrder(Node* root) {
+        queue<Node*> q;
+        vector<int> out;
+        
+        if(root == nullptr)
+            return out;
+        
+        q.push(root);
+        
+        while(!q.empty()) {
+            auto front = q.front();
+            q.pop();
+            out.push_back(front->data);
+            
+            if(front->left)
+                q.push(front->left);
+            if(front->right)
+                q.push(front->right);
+        }
+        
+        return out;
+    }
+};
