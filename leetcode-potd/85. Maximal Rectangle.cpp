8749d8903f3cb114e9cf100c9ac10ec8d5b2d96d
Chakit Gupta <76941922+chakitg@users.noreply.github.com>
2024-04-13 22:30:10 +0530

    Create 85. Maximal Rectangle.cpp

---
+++ leetcode-potd/85. Maximal Rectangle.cpp
@@ -0,0 +1,68 @@
+Given a rows x cols binary matrix filled with 0's and 1's, find the largest rectangle containing only 1's and return its area.
+
+ 
+
+Example 1:
+
+
+Input: matrix = [["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]]
+Output: 6
+Explanation: The maximal rectangle is shown in the above picture.
+Example 2:
+
+Input: matrix = [["0"]]
+Output: 0
+Example 3:
+
+Input: matrix = [["1"]]
+Output: 1
+ 
+
+Constraints:
+
+rows == matrix.length
+cols == matrix[i].length
+1 <= row, cols <= 200
+matrix[i][j] is '0' or '1'.
+
+
+
+
+class Solution {
+public:
+    int largestRectangleArea(vector < int > & histo) {
+    stack < int > st;
+    int maxA = 0;
+    int n = histo.size();
+    for (int i = 0; i <= n; i++) {
+        while (!st.empty() && (i == n || histo[st.top()] >= histo[i])) {
+            int height = histo[st.top()];
+            st.pop();
+            int width;
+            if (st.empty())
+                width = i;
+            else
+                width = i - st.top() - 1;
+            maxA = max(maxA, width * height);
+        }
+        st.push(i);
+    }
+        return maxA;
+    }
+
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        int maxArea = 0;
+        int n = matrix.size();
+        int m = matrix[0].size();
+        vector<int> height(m, 0);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix[i][j] == '1') height[j]++;
+                else height[j] = 0;
+        }
+        int area = largestRectangleArea(height);
+        maxArea = max(maxArea, area);
+         }
+        return maxArea;
+    }
+};
