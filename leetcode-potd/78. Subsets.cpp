90827c58749680402bc309eed097418c5a918187
Chakit Gupta <76941922+chakitg@users.noreply.github.com>
2024-05-21 23:56:29 +0530

    Update 78. Subsets.cpp

--- leetcode-potd/78. Subsets.cpp
+++ leetcode-potd/78. Subsets.cpp
@@ -39,3 +39,33 @@ public:
         return ans;
     }
 };
+
+
+
+
+---------------------------------------------------
+
+
+
+
+ class Solution {
+public:
+    int n;
+    vector<vector<int>> subsets(vector<int>& a) {
+        n = a.size();
+        vector<vector<int>> res;
+        vector<int> curr;
+        backTrack(res,0,curr,a);
+        return res;
+    }
+
+    void backTrack(vector<vector<int>>& res, int ind, vector<int> curr, vector<int>& a) {
+        if(ind==n) {
+            res.push_back(curr);
+            return;
+        }
+        backTrack(res, ind+1, curr, a);
+        curr.push_back(a[ind]);
+        backTrack(res, ind+1, curr, a);
+    }
+};
