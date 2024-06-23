a49ceb9efcf3765de84349d5e87f6c181a7758c2
Chakit Gupta <76941922+chakitg@users.noreply.github.com>
2024-06-23 22:53:42 +0530

    Create 1438. Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit.cpp

---
+++ leetcode-potd/1438. Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit.cpp
@@ -0,0 +1,63 @@
+Given an array of integers nums and an integer limit, return the size of the longest non-empty subarray such that the absolute difference between any two elements of this subarray is less than or equal to limit.
+
+ 
+
+Example 1:
+
+Input: nums = [8,2,4,7], limit = 4
+Output: 2 
+Explanation: All subarrays are: 
+[8] with maximum absolute diff |8-8| = 0 <= 4.
+[8,2] with maximum absolute diff |8-2| = 6 > 4. 
+[8,2,4] with maximum absolute diff |8-2| = 6 > 4.
+[8,2,4,7] with maximum absolute diff |8-2| = 6 > 4.
+[2] with maximum absolute diff |2-2| = 0 <= 4.
+[2,4] with maximum absolute diff |2-4| = 2 <= 4.
+[2,4,7] with maximum absolute diff |2-7| = 5 > 4.
+[4] with maximum absolute diff |4-4| = 0 <= 4.
+[4,7] with maximum absolute diff |4-7| = 3 <= 4.
+[7] with maximum absolute diff |7-7| = 0 <= 4. 
+Therefore, the size of the longest subarray is 2.
+Example 2:
+
+Input: nums = [10,1,2,4,7,2], limit = 5
+Output: 4 
+Explanation: The subarray [2,4,7,2] is the longest since the maximum absolute diff is |2-7| = 5 <= 5.
+Example 3:
+
+Input: nums = [4,2,2,2,4,4,2,2], limit = 0
+Output: 3
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+1 <= nums[i] <= 109
+0 <= limit <= 109
+
+
+
+  class Solution {
+public:
+    int longestSubarray(vector<int>& nums, int limit) {
+        multiset<int>s1;
+          int i,j=0,n=nums.size(),ans=1;
+         
+          
+          for(i=0;i<n;i++)
+          {
+            s1.insert(nums[i]);
+        
+             while(abs(*s1.rbegin()-*s1.begin())>limit)
+             {
+                s1.erase(s1.find(nums[j]));
+                
+                j++;
+             }
+             ans = max(ans, i - j + 1);
+          }
+          return ans;
+
+
+    }
+};
