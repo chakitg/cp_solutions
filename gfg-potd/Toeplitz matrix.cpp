af9a51ea650dc4dbd5b8419be4a8b4e3b3b4a3f6
Chakit Gupta <76941922+chakitg@users.noreply.github.com>
2024-06-27 23:10:39 +0530

    Create Toeplitz matrix.cpp

---
+++ gfg-potd/Toeplitz matrix.cpp
@@ -0,0 +1,48 @@
+A Toeplitz (or diagonal-constant) matrix is a matrix in which each descending diagonal from left to right is constant, i.e., all elements in a diagonal are the same. Given a rectangular matrix mat, your task is to complete the function isToeplitz which returns true if the matrix is Toeplitz otherwise, it returns false.
+
+Examples:
+
+Input:
+mat = [[6, 7, 8],
+       [4, 6, 7],
+       [1, 4, 6]]
+Output: true
+Explanation: The test case represents a 3x3 matrix
+ 6 7 8 
+ 4 6 7 
+ 1 4 6
+Output will be true, as values in all downward diagonals from left to right contain the same elements.
+Input: 
+mat = [[1,2,3],
+       [4,5,6]]
+Output: false
+Explanation: Matrix of order 2x3 will be 1 2 3 4 5 6 Output: false as values in all diagonals are not the same.
+Constraints:
+1<=mat.size,mat[0].size<=40
+0<=mat[i][j]<=100
+
+Expected time complexity: O(n*m)
+Expected space complexity: O(1)
+
+
+
+
+
+
+
+bool isToeplitz(vector<vector<int>> &mat)
+{
+    int n = mat.size();
+    int m = mat[0].size();
+    for (int i = 1; i < n; i++)
+    {
+        for (int j = 1; j < m; j++)
+        {
+            if (mat[i][j] != mat[i - 1][j - 1])
+            {
+                return false;
+            }
+        }
+    }
+    return true;
+}
