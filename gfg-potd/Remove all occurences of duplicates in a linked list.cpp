662f3f4d4986e314ae49264132debb70713f47b1
Chakit Gupta <76941922+chakitg@users.noreply.github.com>
2024-07-04 00:00:58 +0530

    Update Remove all occurences of duplicates in a linked list.cpp

--- gfg-potd/Remove all occurences of duplicates in a linked list.cpp
+++ gfg-potd/Remove all occurences of duplicates in a linked list.cpp
@@ -1,3 +1,25 @@
+Given a sorted linked list, delete all nodes that have duplicate numbers (all occurrences), leaving only numbers that appear once in the original list, and return the head of the modified linked list. 
+
+Examples:
+
+Input: Linked List = 23->28->28->35->49->49->53->53
+Output: 23 35
+Explanation: 
+
+The duplicate numbers are 28, 49 and 53 which are removed from the list.
+Input: Linked List = 11->11->11->11->75->75
+Output: Empty list
+Explanation: 
+
+All the nodes in the linked list have duplicates. Hence the resultant list would be empty.
+Expected Time Complexity: O(n)
+Expected Auxiliary Space: O(1)
+Constraints:
+1 ≤ size(list) ≤ 105
+
+
+
+
 class Solution {
   public:
     Node* removeAllDuplicates(struct Node* head) {
