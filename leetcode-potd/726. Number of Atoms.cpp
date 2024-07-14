61c3626633c867729a5faf466617df7dfd0fded5
Chakit Gupta <76941922+chakitg@users.noreply.github.com>
2024-07-14 23:57:33 +0530

    Create 726. Number of Atoms.cpp

---
+++ leetcode-potd/726. Number of Atoms.cpp
@@ -0,0 +1,105 @@
+Given a string formula representing a chemical formula, return the count of each atom.
+
+The atomic element always starts with an uppercase character, then zero or more lowercase letters, representing the name.
+
+One or more digits representing that element's count may follow if the count is greater than 1. If the count is 1, no digits will follow.
+
+For example, "H2O" and "H2O2" are possible, but "H1O2" is impossible.
+Two formulas are concatenated together to produce another formula.
+
+For example, "H2O2He3Mg4" is also a formula.
+A formula placed in parentheses, and a count (optionally added) is also a formula.
+
+For example, "(H2O2)" and "(H2O2)3" are formulas.
+Return the count of all elements as a string in the following form: the first name (in sorted order), followed by its count (if that count is more than 1), followed by the second name (in sorted order), followed by its count (if that count is more than 1), and so on.
+
+The test cases are generated so that all the values in the output fit in a 32-bit integer.
+
+ 
+
+Example 1:
+
+Input: formula = "H2O"
+Output: "H2O"
+Explanation: The count of elements are {'H': 2, 'O': 1}.
+Example 2:
+
+Input: formula = "Mg(OH)2"
+Output: "H2MgO2"
+Explanation: The count of elements are {'H': 2, 'Mg': 1, 'O': 2}.
+Example 3:
+
+Input: formula = "K4(ON(SO3)2)2"
+Output: "K4N2O14S4"
+Explanation: The count of elements are {'K': 4, 'N': 2, 'O': 14, 'S': 4}.
+ 
+
+Constraints:
+
+1 <= formula.length <= 1000
+formula consists of English letters, digits, '(', and ')'.
+formula is always valid.
+
+
+
+
+class Solution {
+public:
+    string countOfAtoms(string formula) {
+        int n = formula.length();
+        vector<int> mult(n, 1);
+        stack<int> factors;
+        int multFactor = 1;
+        for(int i=n-1; i>=0; --i) {
+            char ch = formula[i];
+            if(ch == ')') {
+                string cnt = "";
+                int k = i+1;
+                while(k<n && isdigit(formula[k])) {
+                    cnt += formula[k++];
+                }
+                if(cnt == "") cnt = "1";
+                int digit = stoi(cnt);
+                factors.push(digit);
+                multFactor *= digit;
+            }
+            else if(ch == '(') {
+                int digit = factors.top();
+                factors.pop();
+                multFactor /= digit;
+            }
+            else if(isupper(ch)) {
+                mult[i] = multFactor;
+            }
+            else continue;
+        }
+        map<string, int> res;
+        for(int i=0; i<n; ++i) {
+            char ch = formula[i];
+            if(!isupper(ch)) continue;
+            multFactor = mult[i];
+            string ele = "";
+            ele += ch;
+            int j = i+1;
+            while(j<n && islower(formula[j])) {
+                ele += formula[j++];
+            }
+            string cnt = "";
+            int k = j;
+            while(k<n && isdigit(formula[k])) {
+                cnt += formula[k++];
+            }
+            if(cnt == "") cnt = "1";
+            int digit = stoi(cnt);
+            res[ele] += digit * multFactor;
+            i = k-1;
+        }
+        string ans = "";
+        for(auto &[ele, digit] :res) {
+            string cnt = to_string(digit);
+            if(cnt == "1") cnt = "";
+            ans += ele + cnt;
+        }
+        return ans;
+    }
+};
