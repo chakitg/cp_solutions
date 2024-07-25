5a8cfe81f46ab1eb44fc8d8a532f7779b9e6d3a4
Chakit Gupta <76941922+chakitg@users.noreply.github.com>
2024-07-25 23:54:47 +0530

    Create 912. Sort an Array.cpp

---
+++ leetcode-potd/912. Sort an Array.cpp
@@ -0,0 +1,59 @@
+Given an array of integers nums, sort the array in ascending order and return it.
+
+You must solve the problem without using any built-in functions in O(nlog(n)) time complexity and with the smallest space complexity possible.
+
+ 
+
+Example 1:
+
+Input: nums = [5,2,3,1]
+Output: [1,2,3,5]
+Explanation: After sorting the array, the positions of some numbers are not changed (for example, 2 and 3), while the positions of other numbers are changed (for example, 1 and 5).
+Example 2:
+
+Input: nums = [5,1,1,2,0,0]
+Output: [0,0,1,1,2,5]
+Explanation: Note that the values of nums are not necessairly unique.
+ 
+
+Constraints:
+
+1 <= nums.length <= 5 * 104
+-5 * 104 <= nums[i] <= 5 * 104
+
+
+
+  class Solution {
+public:
+    void heapify(vector<int>& nums, int n, int i) {
+        int largest = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+        if (left < n && nums[left] > nums[largest]) {
+            largest = left;
+        }
+        if (right < n && nums[right] > nums[largest]) {
+            largest = right;
+        }
+        if (largest != i) {
+            swap(nums[i], nums[largest]);
+            heapify(nums, n, largest);
+        }
+    }
+
+    void heapSort(vector<int>& nums) {
+        int n = nums.size();
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapify(nums, n, i);
+        }
+        for (int i = n - 1; i > 0; i--) {
+            swap(nums[0], nums[i]);
+            heapify(nums, i, 0);
+        }
+    }
+
+    vector<int> sortArray(vector<int>& nums) {
+        heapSort(nums);
+        return nums;
+    }
+};
