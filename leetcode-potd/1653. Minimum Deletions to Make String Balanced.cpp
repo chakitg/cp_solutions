d4d863226be1504542f1e46a7dae8e9a78a67ac1
Chakit Gupta <76941922+chakitg@users.noreply.github.com>
2024-07-30 21:59:50 +0530

    Create 1653. Minimum Deletions to Make String Balanced.cpp

---
+++ leetcode-potd/1653. Minimum Deletions to Make String Balanced.cpp
@@ -0,0 +1,46 @@
+You are given a string s consisting only of characters 'a' and 'b'​​​​.
+
+You can delete any number of characters in s to make s balanced. s is balanced if there is no pair of indices (i,j) such that i < j and s[i] = 'b' and s[j]= 'a'.
+
+Return the minimum number of deletions needed to make s balanced.
+
+ 
+
+Example 1:
+
+Input: s = "aababbab"
+Output: 2
+Explanation: You can either:
+Delete the characters at 0-indexed positions 2 and 6 ("aababbab" -> "aaabbb"), or
+Delete the characters at 0-indexed positions 3 and 6 ("aababbab" -> "aabbbb").
+Example 2:
+
+Input: s = "bbaaaaabb"
+Output: 2
+Explanation: The only solution is to delete the first two characters.
+ 
+
+Constraints:
+
+1 <= s.length <= 105
+s[i] is 'a' or 'b'​​.
+
+
+
+
+
+class Solution {
+public:
+    int minimumDeletions(string s) {
+        int ans=0;
+        int countB=0;
+        for(char c:s){
+            if(c=='b'){
+                countB++;
+            }else{
+                ans=min(ans+1,countB);
+            }
+        }
+        return ans;
+    }
+};
