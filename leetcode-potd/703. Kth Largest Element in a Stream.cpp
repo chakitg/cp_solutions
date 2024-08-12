e892dc18f0dadab711ffea1771e713354b62ae67
Chakit Gupta <76941922+chakitg@users.noreply.github.com>
2024-08-12 23:12:40 +0530

    Create 703. Kth Largest Element in a Stream.cpp

---
+++ leetcode-potd/703. Kth Largest Element in a Stream.cpp
@@ -0,0 +1,67 @@
+Design a class to find the kth largest element in a stream. Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+Implement KthLargest class:
+
+KthLargest(int k, int[] nums) Initializes the object with the integer k and the stream of integers nums.
+int add(int val) Appends the integer val to the stream and returns the element representing the kth largest element in the stream.
+ 
+
+Example 1:
+
+Input
+["KthLargest", "add", "add", "add", "add", "add"]
+[[3, [4, 5, 8, 2]], [3], [5], [10], [9], [4]]
+Output
+[null, 4, 5, 5, 8, 8]
+
+Explanation
+KthLargest kthLargest = new KthLargest(3, [4, 5, 8, 2]);
+kthLargest.add(3);   // return 4
+kthLargest.add(5);   // return 5
+kthLargest.add(10);  // return 5
+kthLargest.add(9);   // return 8
+kthLargest.add(4);   // return 8
+ 
+
+Constraints:
+
+1 <= k <= 104
+0 <= nums.length <= 104
+-104 <= nums[i] <= 104
+-104 <= val <= 104
+At most 104 calls will be made to add.
+It is guaranteed that there will be at least k elements in the array when you search for the kth element.
+
+
+
+
+
+
+class KthLargest {
+public:
+    KthLargest(int k, vector<int>& nums) {
+        this->k=k;
+        for(int num : nums){
+            add(num);
+        }
+    }
+    
+    int add(int val) {
+        minHeap.push(val);
+        //check if minHeap size exceeds given K
+        if(minHeap.size()>k){
+            minHeap.pop();
+        }
+        //return top element which is the k-th largest element
+        return minHeap.top();
+    }
+private:
+    int k;
+    priority_queue<int,vector<int>,greater<int>> minHeap; // minHeap will store the stream 
+};
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest* obj = new KthLargest(k, nums);
+ * int param_1 = obj->add(val);
+ */
