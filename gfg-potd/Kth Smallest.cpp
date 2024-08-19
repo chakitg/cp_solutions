c9586822015faf0a27e71f30a4227a9b415eef25
Chakit Gupta <76941922+chakitg@users.noreply.github.com>
2024-08-19 22:38:43 +0530

    Create Kth Smallest.cpp

---
+++ gfg-potd/Kth Smallest.cpp
@@ -0,0 +1,41 @@
+Given an array arr[] and an integer k where k is smaller than the size of the array, the task is to find the kth smallest element in the given array. It is given that all array elements are distinct.
+
+Follow up: Don't solve it using the inbuilt sort function.
+
+Examples :
+
+Input: arr[] = [7, 10, 4, 3, 20, 15], k = 3
+Output:  7
+Explanation: 3rd smallest element in the given array is 7.
+Input: arr[] = [7, 10, 4, 20, 15], k = 4 
+Output:  15
+Explanation: 4th smallest element in the given array is 15.
+Expected Time Complexity: O(n+(max_element) )
+Expected Auxiliary Space: O(max_element)
+Constraints:
+1 <= arr.size <= 106
+1<= arr[i] <= 106
+1 <= k <= n
+
+
+
+
+
+
+class Solution {
+  public:
+    // arr : given array
+    // k : find kth smallest element and return using this function
+    int kthSmallest(vector<int> &arr, int k) {
+        // Aman Yadav
+         map<int,int> mp;
+       for(int i=0;i<arr.size();i++){
+           mp[arr[i]]++;
+       }
+       for(auto i:mp){
+           k--;
+           if(k==0) return i.first;
+       }
+       return 0;
+    }
+};
