cdd30a4ade6dbf250a018a6e1686a1badc9f242b
Chakit Gupta <76941922+chakitg@users.noreply.github.com>
2024-09-20 23:51:54 +0530

    Create Facing the sun.cpp

---
+++ gfg-potd/Facing the sun.cpp
@@ -0,0 +1,40 @@
+Given an array height representing the heights of buildings. You have to count the buildings that will see the sunrise (Assume the sun rises on the side of the array starting point).
+Note: The height of the building should be strictly greater than the height of the buildings left in order to see the sun.
+
+
+Examples :
+
+Input: height = [7, 4, 8, 2, 9]
+Output: 3
+Explanation: As 7 is the first element, it can see the sunrise. 4 can't see the sunrise as 7 is hiding it. 8 can see. 2 can't see the sunrise. 9 also can see
+the sunrise.
+Input: height = [2, 3, 4, 5]
+Output: 4
+Explanation: As 2 is the first element, it can see the sunrise.  3 can see the sunrise as 2 is not hiding it. Same for 4 and 5, they also can see the sunrise.
+Expected Time Complexity: O(n)
+Expected Auxiliary Space: O(1)
+
+Constraints:
+1 ≤ height.size() ≤ 106
+1 ≤ heighti ≤ 108
+
+
+
+
+class Solution {
+  public:
+    // Returns count buildings that can see sunlight
+    int countBuildings(vector<int> &height) {
+        // code here
+        int ans = 1;
+        int mx = height[0];
+        for(int i = 1; i < height.size(); i++) {
+            if(height[i] > mx) {
+                ans++;
+                mx = height[i];
+            }
+        }
+        return ans;
+        
+    }
+};
