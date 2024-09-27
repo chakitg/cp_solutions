0a1d10d66659db5db9b62a5b642f1cfb4c4bd588
Chakit Gupta <76941922+chakitg@users.noreply.github.com>
2024-09-27 23:36:32 +0530

    Create 731. My Calendar II.cpp

---
+++ leetcode-potd/731. My Calendar II.cpp
@@ -0,0 +1,62 @@
+You are implementing a program to use as your calendar. We can add a new event if adding the event will not cause a triple booking.
+
+A triple booking happens when three events have some non-empty intersection (i.e., some moment is common to all the three events.).
+
+The event can be represented as a pair of integers start and end that represents a booking on the half-open interval [start, end), the range of real numbers x such that start <= x < end.
+
+Implement the MyCalendarTwo class:
+
+MyCalendarTwo() Initializes the calendar object.
+boolean book(int start, int end) Returns true if the event can be added to the calendar successfully without causing a triple booking. Otherwise, return false and do not add the event to the calendar.
+ 
+
+Example 1:
+
+Input
+["MyCalendarTwo", "book", "book", "book", "book", "book", "book"]
+[[], [10, 20], [50, 60], [10, 40], [5, 15], [5, 10], [25, 55]]
+Output
+[null, true, true, true, false, true, true]
+
+Explanation
+MyCalendarTwo myCalendarTwo = new MyCalendarTwo();
+myCalendarTwo.book(10, 20); // return True, The event can be booked. 
+myCalendarTwo.book(50, 60); // return True, The event can be booked. 
+myCalendarTwo.book(10, 40); // return True, The event can be double booked. 
+myCalendarTwo.book(5, 15);  // return False, The event cannot be booked, because it would result in a triple booking.
+myCalendarTwo.book(5, 10); // return True, The event can be booked, as it does not use time 10 which is already double booked.
+myCalendarTwo.book(25, 55); // return True, The event can be booked, as the time in [25, 40) will be double booked with the third event, the time [40, 50) will be single booked, and the time [50, 55) will be double booked with the second event.
+ 
+
+Constraints:
+
+0 <= start < end <= 109
+At most 1000 calls will be made to book.
+
+
+
+
+class MyCalendarTwo {
+public:
+    vector<pair<int, int>> calendar;    
+    vector<pair<int, int>> overlaps;   
+
+    MyCalendarTwo() { }
+
+    bool book(int start, int end) {
+        for (const auto& o : overlaps) {
+            if (max(o.first, start) < min(o.second, end)) {
+                return false;  
+            }
+        }
+
+        for (const auto& c : calendar) {
+            if (max(c.first, start) < min(c.second, end)) {
+                overlaps.push_back({max(c.first, start), min(c.second, end)});
+            }
+        }
+
+        calendar.push_back({start, end});
+        return true;
+    }
+};
