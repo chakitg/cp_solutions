b92803a2dfffa9bbe7c84a3bf8b59e8937bc3173
Chakit Gupta <76941922+chakitg@users.noreply.github.com>
2024-10-25 23:54:43 +0530

    Create 1233. Remove Sub-Folders from the Filesystem.cpp

---
+++ leetcode-potd/1233. Remove Sub-Folders from the Filesystem.cpp
@@ -0,0 +1,85 @@
+Given a list of folders folder, return the folders after removing all sub-folders in those folders. You may return the answer in any order.
+
+If a folder[i] is located within another folder[j], it is called a sub-folder of it. A sub-folder of folder[j] must start with folder[j], followed by a "/". For example, "/a/b" is a sub-folder of "/a", but "/b" is not a sub-folder of "/a/b/c".
+
+The format of a path is one or more concatenated strings of the form: '/' followed by one or more lowercase English letters.
+
+For example, "/leetcode" and "/leetcode/problems" are valid paths while an empty string and "/" are not.
+ 
+
+Example 1:
+
+Input: folder = ["/a","/a/b","/c/d","/c/d/e","/c/f"]
+Output: ["/a","/c/d","/c/f"]
+Explanation: Folders "/a/b" is a subfolder of "/a" and "/c/d/e" is inside of folder "/c/d" in our filesystem.
+Example 2:
+
+Input: folder = ["/a","/a/b/c","/a/b/d"]
+Output: ["/a"]
+Explanation: Folders "/a/b/c" and "/a/b/d" will be removed because they are subfolders of "/a".
+Example 3:
+
+Input: folder = ["/a/b/c","/a/b/ca","/a/b/d"]
+Output: ["/a/b/c","/a/b/ca","/a/b/d"]
+ 
+
+Constraints:
+
+1 <= folder.length <= 4 * 104
+2 <= folder[i].length <= 100
+folder[i] contains only lowercase letters and '/'.
+folder[i] always starts with the character '/'.
+Each folder name is unique.
+
+
+
+
+  class Solution {
+public:
+    vector<string> removeSubfolders(vector<string>& folders) {
+        sort(folders.begin(), folders.end());
+        int n = folders.size();
+        vector<string> ans;
+        map<string, int> mp;
+        for(int i=0; i<n; i++){
+            string s = folders[i];
+            string temp;
+            bool flag = false;
+            int incr=1;
+            //substring generation code....
+            for(int j=0; j<s.size();){
+                int k=j;
+                if(j == 0){
+                    temp.push_back('/');
+                    k++;
+                }
+                while(k<s.size() && s[k]!='/'){
+                    temp.push_back(s[k]);
+                    k++;
+                }
+                if(k < s.size() && s[k] == '/'){
+                    temp.push_back('/');
+                    k++;
+                }
+                j = k;
+                bool x = 0;
+                if(temp[temp.size()-1] == '/'){
+                    temp.pop_back();
+                    x=1;
+                }
+                if(mp.find(temp)!=mp.end()){
+                    flag = true;
+                    break;
+                }
+                if(x)
+                    temp.push_back('/');
+            }
+            //substring gen code ends here.....
+            if(!flag) {
+                mp[s]++;
+                ans.push_back(s);
+            }
+        }
+        return ans;
+    }
+};
