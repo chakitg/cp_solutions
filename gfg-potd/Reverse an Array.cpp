befe496c290cd2db2467d61f75f2b1c3b2b177fe
Chakit Gupta <76941922+chakitg@users.noreply.github.com>
2024-11-17 23:56:37 +0530

    Create Reverse an Array.cpp

---
+++ gfg-potd/Reverse an Array.cpp
@@ -0,0 +1,31 @@
+You are given an array of integers arr[]. Your task is to reverse the given array.
+
+Examples:
+
+Input: arr = [1, 4, 3, 2, 6, 5]
+Output: [5, 6, 2, 3, 4, 1]
+Explanation: The elements of the array are 1 4 3 2 6 5. After reversing the array, the first element goes to the last position, the second element goes to the second last position and so on. Hence, the answer is 5 6 2 3 4 1.
+Input: arr = [4, 5, 2]
+Output: [2, 5, 4]
+Explanation: The elements of the array are 4 5 2. The reversed array will be 2 5 4.
+Input: arr = [1]
+Output: [1]
+Explanation: The array has only single element, hence the reversed array is same as the original.
+Constraints:
+1<=arr.size()<=105
+0<=arr[i]<=105
+
+
+class Solution {
+  public:
+    void reverseArray(vector<int> &arr) {
+        int i=0;
+        int j=arr.size()-1;
+        while(i<=j)
+        {
+            swap(arr[i],arr[j]);
+            i++;
+            j--;
+        }
+    }
+};
