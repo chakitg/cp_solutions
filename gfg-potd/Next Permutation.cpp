7da5b7984b5415d9d9b885ff6bf6d3881d8008e9
Chakit Gupta <76941922+chakitg@users.noreply.github.com>
2024-11-19 22:07:47 +0530

    Create Next Permutation.cpp

---
+++ gfg-potd/Next Permutation.cpp
@@ -0,0 +1,47 @@
+Given an array of integers arr[] representing a permutation, implement the next permutation that rearranges the numbers into the lexicographically next greater permutation. If no such permutation exists, rearrange the numbers into the lowest possible order (i.e., sorted in ascending order). 
+
+Note - A permutation of an array of integers refers to a specific arrangement of its elements in a sequence or linear order.
+
+Examples:
+
+Input: arr = [2, 4, 1, 7, 5, 0]
+Output: [2, 4, 5, 0, 1, 7]
+Explanation: The next permutation of the given array is {2, 4, 5, 0, 1, 7}.
+Input: arr = [3, 2, 1]
+Output: [1, 2, 3]
+Explanation: As arr[] is the last permutation, the next permutation is the lowest one.
+Input: arr = [3, 4, 2, 5, 1]
+Output: [3, 4, 5, 1, 2]
+Explanation: The next permutation of the given array is {3, 4, 5, 1, 2}.
+Constraints:
+1 ≤ arr.size() ≤ 105
+1 ≤ arr[i] ≤ 105
+
+
+
+
+class Solution {
+  public:
+    void nextPermutation(vector<int>& arr) {
+        // code here
+        int n = arr.size();
+        int ind = -1;
+        for (int i = n - 2; i >= 0; i--) {
+            if (arr[i] < arr[i + 1]) {
+                ind = i;
+                break;
+            }
+        }
+        if (ind == -1) {
+            reverse(arr.begin(), arr.end());
+            return;
+        }
+        for (int i = n - 1; i > ind; i--) {
+            if (arr[i] > arr[ind]) {
+                swap(arr[i], arr[ind]);
+                break;
+            }
+        }
+        reverse(arr.begin() + ind + 1, arr.end());
+    }
+};
