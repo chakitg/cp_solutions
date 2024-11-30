1adb8d337d8ea1384bfe1cefdaf847e7b608fe89
Chakit Gupta <76941922+chakitg@users.noreply.github.com>
2024-11-30 23:05:41 +0530

    Create Anagram.cpp

---
+++ gfg-potd/Anagram.cpp
@@ -0,0 +1,33 @@
+Given two strings s1 and s2 consisting of lowercase characters. The task is to check whether two given strings are an anagram of each other or not. An anagram of a string is another string that contains the same characters, only the order of characters can be different. For example, act and tac are an anagram of each other. Strings s1 and s2 can only contain lowercase alphabets.
+
+Note: You can assume both the strings s1 & s2 are non-empty.
+
+Examples :
+
+Input: s1 = "geeks", s2 = "kseeg"
+Output: true
+Explanation: Both the string have same characters with same frequency. So, they are anagrams.
+Input: s1 = "allergy", s2 = "allergic"
+Output: false
+Explanation: Characters in both the strings are not same, so they are not anagrams.
+Input: s1 = "g", s2 = "g"
+Output: true
+Explanation: Character in both the strings are same, so they are anagrams.
+Constraints:
+1 ≤ s1.size(), s2.size() ≤ 105
+
+
+
+class Solution {
+  public:
+    bool areAnagrams(string& s1, string& s2) {
+        if(s1.size()!=s2.size()) return false;
+        vector<int>freq1(26,0);
+        vector<int>freq2(26,0);
+        for(int i=0;i<s1.size();i++){
+            freq1[s1[i]-'a']++;
+            freq2[s2[i]-'a']++;
+        }
+        return freq1==freq2;
+    }
+};
