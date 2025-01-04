7094914480ec92e03041ee97c2f46b649908c718
Chakit Gupta <76941922+chakitg@users.noreply.github.com>
2025-01-05 00:00:08 +0530

    Update 1930. Unique Length-3 Palindromic Subsequences.cpp

--- leetcode-potd/1930. Unique Length-3 Palindromic Subsequences.cpp
+++ leetcode-potd/1930. Unique Length-3 Palindromic Subsequences.cpp
@@ -67,5 +67,35 @@ public:
             if ((--right[s[i + 1]]) == 0) right.erase(s[i + 1]);
         }
         return result;
+
+     
     }
+};
+
+
+
+
+var countPalindromicSubsequence = function(s) {
+    let rightCount = Array(26).fill(0);
+    let leftCount = Array(26).fill(0);
+    let subsequences = new Set();
+    
+    for (let char of s) {
+        rightCount[char.charCodeAt(0) - 'a'.charCodeAt(0)]++;
+    }
+    
+    for (let char of s) {
+        let t = char.charCodeAt(0) - 'a'.charCodeAt(0);
+        rightCount[t]--;
+        
+        for (let j = 0; j < 26; j++) {
+            if (leftCount[j] > 0 && rightCount[j] > 0) {
+                subsequences.add(26 * t + j);
+            }
+        }
+        
+        leftCount[t]++;
+    }
+    
+    return subsequences.size;
 };
