dcb96a47f5feee258c04d42b63483a553a8b97d0
Chakit Gupta <76941922+chakitg@users.noreply.github.com>
2025-03-30 23:51:29 +0530

    Create 763. Partition Labels.js

---
+++ leetcode-potd/763. Partition Labels.js
@@ -0,0 +1,40 @@
+You are given a string s. We want to partition the string into as many parts as possible so that each letter appears in at most one part. For example, the string "ababcc" can be partitioned into ["abab", "cc"], but partitions such as ["aba", "bcc"] or ["ab", "ab", "cc"] are invalid.
+
+Note that the partition is done so that after concatenating all the parts in order, the resultant string should be s.
+
+Return a list of integers representing the size of these parts.
+
+ 
+
+Example 1:
+
+Input: s = "ababcbacadefegdehijhklij"
+Output: [9,7,8]
+Explanation:
+The partition is "ababcbaca", "defegde", "hijhklij".
+This is a partition so that each letter appears in at most one part.
+A partition like "ababcbacadefegde", "hijhklij" is incorrect, because it splits s into less parts.
+Example 2:
+
+Input: s = "eccbbbbdec"
+Output: [10]
+ 
+
+Constraints:
+
+1 <= s.length <= 500
+s consists of lowercase English letters.
+
+
+
+const partitionLabels = (s) => {
+    const sizes = [];
+    for (let left = 0, right = 0, shift = 0; left < s.length; left++) {
+        right = Math.max(right, s.lastIndexOf(s[left]));
+        if (left === right) {
+            sizes.push(left + 1 - shift);
+            shift = left + 1;
+        }
+    }
+    return sizes;
+};
