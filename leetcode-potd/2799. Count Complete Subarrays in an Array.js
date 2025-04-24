16632b9117121539d8444ae5a4bde3c0e967ee4d
Chakit Gupta <76941922+chakitg@users.noreply.github.com>
2025-04-25 00:48:53 +0530

    Create 2799. Count Complete Subarrays in an Array.js

---
+++ leetcode-potd/2799. Count Complete Subarrays in an Array.js
@@ -0,0 +1,45 @@
+You are given an array nums consisting of positive integers.
+
+We call a subarray of an array complete if the following condition is satisfied:
+
+The number of distinct elements in the subarray is equal to the number of distinct elements in the whole array.
+Return the number of complete subarrays.
+
+A subarray is a contiguous non-empty part of an array.
+
+ 
+
+Example 1:
+
+Input: nums = [1,3,1,2,2]
+Output: 4
+Explanation: The complete subarrays are the following: [1,3,1,2], [1,3,1,2,2], [3,1,2] and [3,1,2,2].
+Example 2:
+
+Input: nums = [5,5,5,5]
+Output: 10
+Explanation: The array consists only of the integer 5, so any subarray is complete. The number of subarrays that we can choose is 10.
+ 
+
+Constraints:
+
+1 <= nums.length <= 1000
+1 <= nums[i] <= 2000
+
+
+
+
+const countCompleteSubarrays = (nums) => {
+    const countDistincts = new Set(nums).size;
+    let countComplete = 0;
+    for (let i = 0; i < nums.length - countDistincts + 1; i++) {
+        for (let j = i, subarraySet = new Set(); j < nums.length; j++) {
+            subarraySet.add(nums[j]);
+            if (subarraySet.size === countDistincts) {
+                countComplete += nums.length - j;
+                break;
+            }
+        }
+    }
+    return countComplete;
+};
