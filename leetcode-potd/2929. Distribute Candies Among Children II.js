553d7751ea463bc1269b0035dfb42771444f27d7
Chakit Gupta <76941922+chakitg@users.noreply.github.com>
2025-06-01 23:53:25 +0530

    Create 2929. Distribute Candies Among Children II.js

---
+++ leetcode-potd/2929. Distribute Candies Among Children II.js
@@ -0,0 +1,28 @@
+You are given two positive integers n and limit.
+
+Return the total number of ways to distribute n candies among 3 children such that no child gets more than limit candies.
+
+ 
+
+Example 1:
+
+Input: n = 5, limit = 2
+Output: 3
+Explanation: There are 3 ways to distribute 5 candies such that no child gets more than 2 candies: (1, 2, 2), (2, 1, 2) and (2, 2, 1).
+Example 2:
+
+Input: n = 3, limit = 3
+Output: 10
+Explanation: There are 10 ways to distribute 3 candies such that no child gets more than 3 candies: (0, 0, 3), (0, 1, 2), (0, 2, 1), (0, 3, 0), (1, 0, 2), (1, 1, 1), (1, 2, 0), (2, 0, 1), (2, 1, 0) and (3, 0, 0).
+ 
+
+Constraints:
+
+1 <= n <= 106
+1 <= limit <= 106
+
+
+
+const progression = f = n => n > 0 ? n * (n - 1) / 2 : 0;
+const distributeCandies = (n, l) => 
+    f(n + 2) + f(n - 2 * l) * 3 - f(n - 3 * l - 1) - f(n - l + 1) * 3;
